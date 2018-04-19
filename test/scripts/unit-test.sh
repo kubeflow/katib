e4eaa0d63f656d8dc296b0dbc43d4be487bfb9a2
Ce Gao <gaoce@caicloud.io>
2018-04-19 21:45:12 +0800

    unit-test: Add ln -s
    Signed-off-by: Ce Gao <gaoce@caicloud.io>

--- test/scripts/unit-test.sh
+++ test/scripts/unit-test.sh
@@ -30,6 +30,10 @@ VERSION=$(git describe --tags --always --dirty)
 echo "Activating service-account"
 gcloud auth activate-service-account --key-file=${GOOGLE_APPLICATION_CREDENTIALS}
 
+echo "Create symlink to GOPATH"
+mkdir -p ${GOPATH}/src/github.com/${REPO_OWNER}
+ln -s ${PWD} ${GO_DIR}
+
 echo "Run unit test cases"
 cd ${GO_DIR}
 go test ./...
