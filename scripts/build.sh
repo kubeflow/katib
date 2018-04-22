95f2bd7583c53f7f731f32a37f7c3dbe0a503d94
Ce Gao <gaoce@caicloud.io>
2018-04-22 13:27:29 +0800

    build.sh: Remove comments
    Signed-off-by: Ce Gao <gaoce@caicloud.io>

--- scripts/build.sh
+++ scripts/build.sh
@@ -24,12 +24,25 @@ CMD_PREFIX="cmd"
 SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/..
 
 cd ${SCRIPT_ROOT}
-# docker build -t ${PREFIX}/vizier-core -f ${CMD_PREFIX}/manager/Dockerfile .
-# docker build -t ${PREFIX}/suggestion-random -f suggestion/random/Dockerfile .
-# docker build -t ${PREFIX}/suggestion-grid -f suggestion/grid/Dockerfile .
-# docker build -t ${PREFIX}/suggestion-hyperband -f suggestion/hyperband/Dockerfile .
+
+echo "Building core image..."
+docker build -t ${PREFIX}/vizier-core -f ${CMD_PREFIX}/manager/Dockerfile .
+
+echo "Building suggestion images..."
+docker build -t ${PREFIX}/suggestion-random -f ${CMD_PREFIX}/suggestion/random/Dockerfile .
+docker build -t ${PREFIX}/suggestion-grid -f ${CMD_PREFIX}/suggestion/grid/Dockerfile .
+docker build -t ${PREFIX}/suggestion-hyperband -f ${CMD_PREFIX}/suggestion/hyperband/Dockerfile .
+docker build -t ${PREFIX}/suggestion-bayesianoptimization -f ${CMD_PREFIX}/suggestion/bayesianoptimization/Dockerfile .
+
+echo "Building earlystopping images..."
 docker build -t ${PREFIX}/earlystopping-medianstopping -f ${CMD_PREFIX}/earlystopping/medianstopping/Dockerfile .
+
+echo "Building dlk manager image..."
 docker build -t ${PREFIX}/dlk-manager -f dlk/Dockerfile .
+
+echo "Building UI image..."
 docker build -t ${PREFIX}/katib-frontend -f modeldb/Dockerfile .
+
+echo "Building CLI image..."
 docker build -t ${PREFIX}/katib-cli -f ${CMD_PREFIX}/cli/Dockerfile .
 cd - > /dev/null
