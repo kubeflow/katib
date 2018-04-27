d0455f0d685e9c1fcc0b8be449e5c6dda9f8582c
Yingbo <libbyandhelen@163.com>
2018-04-26 18:07:31 -0700

    remove duplicate test script

--- test/scripts/run-tests.sh
+++ test/scripts/run-tests.sh
@@ -48,7 +48,6 @@ sed -i -e "s@image: katib\/suggestion-grid@image: ${REGISTRY}\/${REPO_NAME}\/sug
 sed -i -e "s@image: katib\/suggestion-hyperband@image: ${REGISTRY}\/${REPO_NAME}\/suggestion-hyperband:${VERSION}@" manifests/vizier/suggestion/hyperband/deployment.yaml
 sed -i -e "s@image: katib\/suggestion-bayesianoptimization@image: ${REGISTRY}\/${REPO_NAME}\/suggestion-bayesianoptimization:${VERSION}@" manifests/vizier/suggestion/bayesianoptimization/deployment.yaml
 sed -i -e "s@image: katib\/suggestion-cma@image: ${REGISTRY}\/${REPO_NAME}\/suggestion-cma:${VERSION}@" manifests/vizier/suggestion/cma/deployment.yaml
-sed -i -e "s@image: katib\/suggestion-cma@image: ${REGISTRY}\/${REPO_NAME}\/suggestion-cma:${VERSION}@" manifests/vizier/suggestion/cma/deployment.yaml
 sed -i -e "s@image: katib\/earlystopping-medianstopping@image: ${REGISTRY}\/${REPO_NAME}\/earlystopping-medianstopping:${VERSION}@" manifests/vizier/earlystopping/medianstopping/deployment.yaml
 sed -i -e "s@image: katib\/dlk-manager@image: ${REGISTRY}\/${REPO_NAME}\/dlk-manager:${VERSION}@" manifests/dlk/deployment.yaml
 sed -i -e "s@image: katib\/katib-frontend@image: ${REGISTRY}\/${REPO_NAME}\/katib-frontend:${VERSION}@" manifests/modeldb/frontend/deployment.yaml
