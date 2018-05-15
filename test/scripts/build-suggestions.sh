260a394010c8b3284536a855b7f346d065b06bd3
YujiOshima <yuji.oshima0x3fd@gmail.com>
2018-05-16 00:28:23 +0900

    use different path for each component
    Signed-off-by: YujiOshima <yuji.oshima0x3fd@gmail.com>

--- test/scripts/build-suggestions.sh
+++ test/scripts/build-suggestions.sh
@@ -23,7 +23,7 @@ set -o pipefail
 export PATH=${GOPATH}/bin:/usr/local/go/bin:${PATH}
 REGISTRY="${GCP_REGISTRY}"
 PROJECT="${GCP_PROJECT}"
-GO_DIR=${GOPATH}/src/github.com/${REPO_OWNER}/${REPO_NAME}
+GO_DIR=${GOPATH}/src/github.com/${REPO_OWNER}/${REPO_NAME}-suggestions
 VERSION=$(git describe --tags --always --dirty)
 
 echo "Activating service-account"
