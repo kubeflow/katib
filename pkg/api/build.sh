b4b54df2d18910e0d9b0c501c38c45d102f979a6
Mayank Juneja <mjuneja@lyft.com>
2018-08-06 15:50:09 +0530

    update API build script

--- pkg/api/build.sh
+++ pkg/api/build.sh
@@ -1,1 +1,3 @@
 docker run -it --rm -v $PWD:$(pwd) -w $(pwd) znly/protoc --python_out=plugins=grpc:./python --go_out=plugins=grpc:. -I. api.proto
+docker run -it --rm -v $PWD:$(pwd) -w $(pwd) znly/protoc --grpc-gateway_out=logtostderr=true:. -I. api.proto
+docker run -it --rm -v $PWD:$(pwd) -w $(pwd) znly/protoc --swagger_out=logtostderr=true:. -I. api.proto
