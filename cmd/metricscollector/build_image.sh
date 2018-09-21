2b994f5b37711b5979bb6c24159af80dc521573a
Jeremy Lewi <jeremy@lewi.us>
2018-09-20 22:42:08 -0700

    Add build_image.sh files.

---
+++ cmd/metricscollector/build_image.sh
@@ -0,0 +1,25 @@
+
+#!/bin/bash
+#
+# A simple script to build the Docker images.
+# This is intended to be invoked as a step in Argo to build the docker image.
+#
+# build_image.sh ${DOCKERFILE} ${IMAGE} ${TAG} (optional | ${TEST_REGISTRY})
+set -ex
+
+DOCKERFILE=$1
+CONTEXT_DIR=$(dirname "$DOCKERFILE")
+IMAGE=$2
+TAG=$3
+
+# Wait for the Docker daemon to be available.
+until docker ps
+do sleep 3
+done
+
+docker build -t ${TAG} -f ${DOCKERFILE} ${CONTEXT_DIR}
+
+gcloud auth activate-service-account --key-file=${GOOGLE_APPLICATION_CREDENTIALS}
+gcloud docker -- push "${IMAGE}:${TAG}"
+docker tag "${IMAGE}:${TAG}" "${IMAGE}:latest"
+gcloud docker -- push "${IMAGE}:latest"
