a4b50c9bb988372128baf290b26a1ae46184b9f9
YujiOshima <yuji.oshima0x3fd@gmail.com>
2018-10-17 13:40:36 +0900

    eval req count after status check
    Signed-off-by: YujiOshima <yuji.oshima0x3fd@gmail.com>

--- pkg/controller/studyjobcontroller/studyjob_controller.go
+++ pkg/controller/studyjobcontroller/studyjob_controller.go
@@ -477,13 +477,12 @@ func (r *ReconcileStudyJobController) checkStatus(instance *katibv1alpha1.StudyJ
 			log.Printf("Check Goal failed %v", err)
 		}
 	}
-	if instance.Status.SuggestionCount > 0 && instance.Status.SuggestionCount > instance.Spec.RequestCount {
-		log.Printf("Study %s reached the request count. It is completed", instance.Status.StudyID)
-		instance.Status.Condition = katibv1alpha1.ConditionCompleted
-		update = true
-		nextSuggestionSchedule = false
-	}
 	if nextSuggestionSchedule {
+		if instance.Status.SuggestionCount > 0 && instance.Status.SuggestionCount > instance.Spec.RequestCount {
+			log.Printf("Study %s reached the request count. It is completed", instance.Status.StudyID)
+			instance.Status.Condition = katibv1alpha1.ConditionCompleted
+			return true, nil
+		}
 		return r.getAndRunSuggestion(instance, c, ns)
 	} else {
 		return update, nil
