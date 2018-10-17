1e14d3c4bb0cd03b50fcd702d6ba05e1cedbcbfa
oshima <yuji.oshima0x3fd@gmail.com>
2018-10-17 15:30:02 +0900

    Fix reqest count (#214)
    * fix manifest examples
    
    Signed-off-by: YujiOshima <yuji.oshima0x3fd@gmail.com>
    
    * fix MetricsCollector instance
    
    Signed-off-by: YujiOshima <yuji.oshima0x3fd@gmail.com>
    
    * eval req count after status check
    
    Signed-off-by: YujiOshima <yuji.oshima0x3fd@gmail.com>
    
    * fix cont check when ReqestCount is not set
    
    Signed-off-by: YujiOshima <yuji.oshima0x3fd@gmail.com>

--- pkg/controller/studyjobcontroller/studyjob_controller.go
+++ pkg/controller/studyjobcontroller/studyjob_controller.go
@@ -483,13 +483,12 @@ func (r *ReconcileStudyJobController) checkStatus(instance *katibv1alpha1.StudyJ
 			log.Printf("Check Goal failed %v", err)
 		}
 	}
-	if instance.Status.SuggestionCount > 0 && instance.Status.SuggestionCount > instance.Spec.RequestCount {
-		log.Printf("Study %s reached the request count. It is completed", instance.Status.StudyID)
-		instance.Status.Condition = katibv1alpha1.ConditionCompleted
-		update = true
-		nextSuggestionSchedule = false
-	}
 	if nextSuggestionSchedule {
+		if instance.Spec.RequestCount > 0 && instance.Status.SuggestionCount > instance.Spec.RequestCount {
+			log.Printf("Study %s reached the request count. It is completed", instance.Status.StudyID)
+			instance.Status.Condition = katibv1alpha1.ConditionCompleted
+			return true, nil
+		}
 		return r.getAndRunSuggestion(instance, c, ns)
 	} else {
 		return update, nil
