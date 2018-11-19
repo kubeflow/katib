55b78a5d6a7426aa8b6b241788b4e8a13326407d
andreyvelich <andrey.velichkevich@gmail.com>
2018-11-19 09:40:10 -0800

    Change deploy for Minikube Example

--- examples/MinikubeDemo/deploy.sh
+++ examples/MinikubeDemo/deploy.sh
@@ -9,6 +9,7 @@ kubectl apply -f ../../manifests/modeldb/backend
 kubectl apply -f ../../manifests/modeldb/frontend
 kubectl apply -f ../../manifests/vizier/db
 kubectl apply -f ../../manifests/vizier/core
+kubectl apply -f ../../manifests/vizier/ui
 kubectl apply -f ../../manifests/vizier/suggestion/random
 kubectl apply -f ../../manifests/vizier/suggestion/grid
 kubectl apply -f ../../manifests/vizier/suggestion/hyperband
