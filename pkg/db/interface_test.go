kubeflow/katib

commit 67e94c7697bd53f5e61bb9cdd4d9fe3a0c917738

Set MYSQL_ROOT_PASSWORD via Secret (#253)
* Set randomly generated MYSQL_ROOT_PASSWORD via Secret

Signed-off-by: Koichiro Den <den@valinux.co.jp>

* Seperate manifest for MYSQL_ROOT_PASSWORD, "test" being set by default

Signed-off-by: Koichiro Den <den@valinux.co.jp>

* Update run-tests.sh

Fixes: 5312459c28f7 ("Set randomly generated MYSQL_ROOT_PASSWORD via Secret")

Signed-off-by: Koichiro Den <den@valinux.co.jp>

manifests/vizier/db/deployment.yaml
@@ -20,7 +20,10 @@ spec:
         image: mysql:8.0.3
         env:
           - name: MYSQL_ROOT_PASSWORD
-            value: "test"
+            valueFrom:
+              secretKeyRef:
+                name: vizier-db-secrets
+                key: MYSQL_ROOT_PASSWORD
           - name: MYSQL_ALLOW_EMPTY_PASSWORD
             value: "true"
           - name: MYSQL_DATABASE

manifests/vizier/db/secret.yaml
@@ -1,0 +1,8 @@
+apiVersion: v1
+kind: Secret
+type: Opaque
+metadata:
+  name: vizier-db-secrets
+  namespace: katib
+data:
+  MYSQL_ROOT_PASSWORD: test

pkg/db/interface.go
@@ -9,6 +9,7 @@ import (
 	"log"
 	"math/big"
 	"math/rand"
+	"os"
 	"strings"
 	"time"
 
@@ -19,7 +20,7 @@ import (
 
 const (
 	dbDriver     = "mysql"
-	dbName       = "root:test@tcp(vizier-db:3306)/vizier"
+	dbNameTmpl   = "root:%s@tcp(vizier-db:3306)/vizier"
 	mysqlTimeFmt = "2006-01-02 15:04:05.999999"
 )
 
@@ -76,6 +77,19 @@ type dbConn struct {
 
 var rs1Letters = []rune("abcdefghijklmnopqrstuvwxyz")
 
+func getDbName() string {
+	dbPass := os.Getenv("MYSQL_ROOT_PASSWORD")
+	if dbPass == "" {
+		log.Printf("WARN: Env var MYSQL_ROOT_PASSWORD is empty. Falling back to \"test\".")
+
+		// For backward compatibility, e.g. in case that all but vizier-core
+		// is older ones so we do not have Secret nor upgraded vizier-db.
+		dbPass = "test"
+	}
+
+	return fmt.Sprintf(dbNameTmpl, dbPass)
+}
+
 func NewWithSQLConn(db *sql.DB) VizierDBInterface {
 	d := new(dbConn)
 	d.db = db
@@ -91,7 +105,7 @@ func NewWithSQLConn(db *sql.DB) VizierDBInterface {
 }
 
 func New() VizierDBInterface {
-	db, err := sql.Open(dbDriver, dbName)
+	db, err := sql.Open(dbDriver, getDbName())
 	if err != nil {
 		log.Fatalf("DB open failed: %v", err)
 	}

manifests/vizier/core/deployment.yaml
@@ -19,6 +19,12 @@ spec:
       containers:
       - name: vizier-core
         image: katib/vizier-core
+        env:
+          - name: MYSQL_ROOT_PASSWORD
+            valueFrom:
+              secretKeyRef:
+                name: vizier-db-secrets
+                key: MYSQL_ROOT_PASSWORD
         command:
           - './vizier-manager'
         ports:

test/scripts/run-tests.sh
@@ -69,7 +69,7 @@ sed -i -e "s@image: katib\/suggestion-grid@image: ${REGISTRY}\/${REPO_NAME}\/sug
 sed -i -e "s@image: katib\/suggestion-hyperband@image: ${REGISTRY}\/${REPO_NAME}\/suggestion-hyperband:${VERSION}@" manifests/vizier/suggestion/hyperband/deployment.yaml
 sed -i -e "s@image: katib\/suggestion-bayesianoptimization@image: ${REGISTRY}\/${REPO_NAME}\/suggestion-bayesianoptimization:${VERSION}@" manifests/vizier/suggestion/bayesianoptimization/deployment.yaml
 sed -i -e "s@image: katib\/earlystopping-medianstopping@image: ${REGISTRY}\/${REPO_NAME}\/earlystopping-medianstopping:${VERSION}@" manifests/vizier/earlystopping/medianstopping/deployment.yaml
-sed -i -e "31,37d" manifests/vizier/db/deployment.yaml
+sed -i -e '/volumeMounts:/,$d' manifests/vizier/db/deployment.yaml
 
 cat manifests/vizier/core/deployment.yaml
 ./scripts/deploy.sh

commit 72a0fc0fd3cb29e6459025f118a6ce725076d23c

Prevent pod restarts caused by slow db boot (#261)
* Add readinessProbe for vizier-db

Signed-off-by: Koichiro Den <den@valinux.co.jp>

* Fix MYSQL_ROOT_PASSWORD

Fixes: 67e94c7697bd ("Set MYSQL_ROOT_PASSWORD via Secret (#253)")
Signed-off-by: Koichiro Den <den@valinux.co.jp>

* Add simple loop to wait for DB connection successfully opened

Signed-off-by: Koichiro Den <den@valinux.co.jp>

manifests/vizier/db/deployment.yaml
@@ -31,6 +31,15 @@ spec:
         ports:
         - name: dbapi
           containerPort: 3306
+        readinessProbe:
+          exec:
+            command:
+            - "/bin/bash"
+            - "-c"
+            - "mysql -D $$MYSQL_DATABASE -p$$MYSQL_ROOT_PASSWORD -e 'SELECT 1'"
+          initialDelaySeconds: 5
+          periodSeconds: 2
+          timeoutSeconds: 1
         volumeMounts:
         - name: katib-mysql
           mountPath: /var/lib/mysql

manifests/vizier/db/secret.yaml
@@ -5,4 +5,4 @@ metadata:
   name: vizier-db-secrets
   namespace: kubeflow
 data:
-  MYSQL_ROOT_PASSWORD: test
+  MYSQL_ROOT_PASSWORD: dGVzdA== # "test"

pkg/db/interface.go
@@ -22,6 +22,9 @@ const (
 	dbDriver     = "mysql"
 	dbNameTmpl   = "root:%s@tcp(vizier-db:3306)/vizier"
 	mysqlTimeFmt = "2006-01-02 15:04:05.999999"
+
+	connectInterval = 5 * time.Second
+	connectTimeout  = 60 * time.Second
 )
 
 type GetWorkerLogOpts struct {
@@ -90,6 +93,26 @@ func getDbName() string {
 	return fmt.Sprintf(dbNameTmpl, dbPass)
 }
 
+func openSQLConn(driverName string, dataSourceName string, interval time.Duration,
+	timeout time.Duration) (*sql.DB, error) {
+	ticker := time.NewTicker(interval)
+	defer ticker.Stop()
+
+	timeoutC := time.After(timeout)
+	for {
+		select {
+		case <-ticker.C:
+			if db, err := sql.Open(driverName, dataSourceName); err == nil {
+				if err = db.Ping(); err == nil {
+					return db, nil
+				}
+			}
+		case <-timeoutC:
+			return nil, fmt.Errorf("Timeout waiting for DB conn successfully opened.")
+		}
+	}
+}
+
 func NewWithSQLConn(db *sql.DB) VizierDBInterface {
 	d := new(dbConn)
 	d.db = db
@@ -105,7 +128,7 @@ func NewWithSQLConn(db *sql.DB) VizierDBInterface {
 }
 
 func New() VizierDBInterface {
-	db, err := sql.Open(dbDriver, getDbName())
+	db, err := openSQLConn(dbDriver, getDbName(), connectInterval, connectTimeout)
 	if err != nil {
 		log.Fatalf("DB open failed: %v", err)
 	}

pkg/db/interface_test.go
@@ -64,6 +64,25 @@ func TestMain(m *testing.M) {
 	os.Exit(m.Run())
 }
 
+func TestOpenSQLConn(t *testing.T) {
+	_, _, err := sqlmock.New()
+	if err != nil {
+		fmt.Printf("error opening db: %v\n", err)
+		os.Exit(1)
+	}
+	mysqlAddr := os.Getenv("TEST_MYSQL")
+	if mysqlAddr != "" {
+		_, err := openSQLConn("mysql", "root:test123@tcp("+mysqlAddr+")/vizier", time.Second, 3*time.Second)
+		if err != nil {
+			t.Errorf("openSQLConn error: %v", err)
+		}
+	}
+	_, err = openSQLConn("mysql", "root:test123@tcp(dummy)/vizier", time.Second, 3*time.Second)
+	if err.Error() != "Timeout waiting for DB conn successfully opened." {
+		t.Errorf("openSQLConn should timeout but got error: %v", err)
+	}
+}
+
 func TestCreateStudy(t *testing.T) {
 	var in api.StudyConfig
 	in.ParameterConfigs = new(api.StudyConfig_ParameterConfigs)