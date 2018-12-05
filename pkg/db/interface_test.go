kubeflow/katib

commit 61ac5607353e2b3b38855597de5801a788341e08

Add SelectOne() to VizierDBInterface
Signed-off-by: Koichiro Den <den@valinux.co.jp>

pkg/db/interface_test.go
@@ -12,7 +12,6 @@ import (
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/golang/protobuf/jsonpb"
-	"gopkg.in/DATA-DOG/go-sqlmock.v1"
 
 	api "github.com/kubeflow/katib/pkg/api"
 )
@@ -48,6 +47,10 @@ func TestMain(m *testing.M) {
 	mock.ExpectExec("CREATE TABLE IF NOT EXISTS suggestion_param").WithArgs().WillReturnResult(sqlmock.NewResult(1, 1))
 	mock.ExpectExec("CREATE TABLE IF NOT EXISTS earlystop_param").WithArgs().WillReturnResult(sqlmock.NewResult(1, 1))
 	dbInterface.DBInit()
+	err = dbInterface.SelectOne()
+	if err != nil {
+		fmt.Printf("error `SELECT 1` probing: %v\n", err)
+	}
 
 	mysqlAddr := os.Getenv("TEST_MYSQL")
 	if mysqlAddr != "" {

pkg/db/db_init.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"fmt"
 	"log"
 )
 
@@ -93,4 +94,13 @@ func (d *dbConn) DBInit() {
 	if err != nil {
 		log.Fatalf("Error creating earlystop_param table: %v", err)
 	}
+}
+
+func (d *dbConn) SelectOne() error {
+	db := d.db
+	_, err := db.Exec(`SELECT 1`)
+	if err != nil {
+		return fmt.Errorf("Error `SELECT 1` probing: %v", err)
+	}
+	return nil
 }

pkg/db/interface.go
@@ -44,6 +44,8 @@ type WorkerLog struct {
 
 type VizierDBInterface interface {
 	DBInit()
+	SelectOne() error
+
 	GetStudyConfig(string) (*api.StudyConfig, error)
 	GetStudyList() ([]string, error)
 	CreateStudy(*api.StudyConfig) (string, error)

commit 25012d46e6c12b2dff0df8325978ca597168b661

Update test codebase
Fixes: 61ac5607353 ("Add SelectOne() to VizierDBInterface")
Signed-off-by: Koichiro Den <den@valinux.co.jp>

pkg/mock/db/db.go
@@ -78,6 +78,12 @@ func (m *MockVizierDBInterface) DBInit() {
 	m.ctrl.Call(m, "DBInit")
 }
 
+func (m *MockVizierDBInterface) SelectOne() error {
+	ret := m.ctrl.Call(m, "SelectOne")
+	ret0, _ := ret[0].(error)
+	return ret0
+}
+
 // DBInit indicates an expected call of DBInit
 func (mr *MockVizierDBInterfaceMockRecorder) DBInit() *gomock.Call {
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "DBInit", reflect.TypeOf((*MockVizierDBInterface)(nil).DBInit))

pkg/db/interface_test.go
@@ -12,6 +12,7 @@ import (
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/golang/protobuf/jsonpb"
+	"gopkg.in/DATA-DOG/go-sqlmock.v1"
 
 	api "github.com/kubeflow/katib/pkg/api"
 )
@@ -37,7 +38,10 @@ func TestMain(m *testing.M) {
 		os.Exit(1)
 	}
 	//mock.ExpectBegin()
-	dbInterface = NewWithSQLConn(db)
+	dbInterface, err = NewWithSQLConn(db)
+	if err != nil {
+		fmt.Printf("error NewWithSQLConn: %v\n", err)
+	}
 	mock.ExpectExec("CREATE TABLE IF NOT EXISTS studies").WithArgs().WillReturnResult(sqlmock.NewResult(1, 1))
 	mock.ExpectExec("CREATE TABLE IF NOT EXISTS study_permissions").WithArgs().WillReturnResult(sqlmock.NewResult(1, 1))
 	mock.ExpectExec("CREATE TABLE IF NOT EXISTS trials").WithArgs().WillReturnResult(sqlmock.NewResult(1, 1))
@@ -55,12 +59,17 @@ func TestMain(m *testing.M) {
 	mysqlAddr := os.Getenv("TEST_MYSQL")
 	if mysqlAddr != "" {
 		mysql, err := sql.Open("mysql", "root:test123@tcp("+mysqlAddr+")/vizier")
-
 		if err != nil {
 			fmt.Printf("error opening db: %v\n", err)
 			os.Exit(1)
 		}
-		mysqlInterface = NewWithSQLConn(mysql)
+
+		mysqlInterface, err = NewWithSQLConn(mysql)
+		if err != nil {
+			fmt.Printf("error initializing db interface: %v\n", err)
+			os.Exit(1)
+		}
+
 		mysqlInterface.DBInit()
 	}
 