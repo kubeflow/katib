4be865eae7e09b57a4ec2ec13f43bd1f6b98a375
ytetra <42131278+ytetra@users.noreply.github.com>
2018-12-17 00:43:43 +0900

    fix deploy (#284)

--- examples/MinikubeDemo/deploy.sh
+++ examples/MinikubeDemo/deploy.sh
@@ -2,18 +2,4 @@
 set -x
 set -e
 minikube start --disk-size 50g --memory 4096 --cpus 4
-kubectl apply -f ../../manifests/0-namespace.yaml
-kubectl apply -f ../../manifests/pv
-kubectl apply -f ../../manifests/vizier/db
-kubectl apply -f ../../manifests/vizier/core
-kubectl apply -f ../../manifests/vizier/ui
-kubectl apply -f ../../manifests/vizier/suggestion/random
-kubectl apply -f ../../manifests/vizier/suggestion/grid
-kubectl apply -f ../../manifests/vizier/suggestion/hyperband
-kubectl apply -f ../../manifests/vizier/earlystopping/medianstopping
-kubectl apply -f ../../manifests/studyjobcontroller/crd.yaml
-kubectl apply -f ../../manifests/studyjobcontroller/rbac.yaml
-kubectl apply -f ../../manifests/studyjobcontroller/mcrbac.yaml
-kubectl apply -f ../../manifests/studyjobcontroller/workerConfigMap.yaml
-kubectl apply -f ../../manifests/studyjobcontroller/metricsControllerConfigMap.yaml
-kubectl apply -f ../../manifests/studyjobcontroller/studyjobcontroller.yaml
+bash ../../scripts/deploy.sh
