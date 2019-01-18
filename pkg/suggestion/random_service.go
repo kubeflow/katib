f5a5d83c51c7bc212bccb3dd86012a8d2bd6c187
andreyvelich <andrey.velichkevich@gmail.com>
2019-01-17 21:45:40 -0800

    Add JobType in all services

--- pkg/suggestion/random_service.go
+++ pkg/suggestion/random_service.go
@@ -42,6 +42,7 @@ func (s *RandomSuggestService) GetSuggestions(ctx context.Context, in *api.GetSu
 	c := api.NewManagerClient(conn)
 	screq := &api.GetStudyRequest{
 		StudyId: in.StudyId,
+		JobType: "HP",
 	}
 	scr, err := c.GetStudy(ctx, screq)
 	if err != nil {
