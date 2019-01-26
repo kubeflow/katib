eefb1e6ff31ffa512b65cf1d8ced88244d43f501
DeeperMind <1155077043@link.cuhk.edu.hk>
2019-01-25 18:39:14 -0800

    Training Container with Model Constructor for cifar10

---
+++ examples/NAS-training-containers/cifar10/RunTrial.py
@@ -0,0 +1,52 @@
+import keras
+import numpy as np
+from keras.datasets import cifar10
+from ModelConstructor import ModelConstructor
+from keras.utils import to_categorical
+import argparse
+import time
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='TrainingContainer')
+    parser.add_argument('--architecture', type=str, default="", metavar='N',
+                        help='architecture of the neural network')
+    parser.add_argument('--nn_config', type=str, default="", metavar='N',
+                        help='configurations and search space embeddings')
+    args = parser.parse_args()
+
+    arch = args.architecture.replace("\'", "\"")
+    print(">>> arch received by trial")
+    print(arch)
+    nn_config = args.nn_config.replace("\'", "\"")
+    print(">>> nn_config received by trial")
+    print(nn_config)
+
+    constructor = ModelConstructor(arch, nn_config)
+    test_model = constructor.build_model()
+
+    test_model.summary()
+    test_model.compile(loss=keras.losses.categorical_crossentropy,
+                       optimizer=keras.optimizers.Adam(lr=1e-3, decay=1e-4),
+                       metrics=['accuracy'])
+
+    x_train, y_train, x_test, y_test = cifar10.load_data()
+    x_train = x_train.astype('float32')
+    x_test = x_test.astype('float32')
+    x_train /= 255
+    x_test /= 255
+    y_train = to_categorical(y_train)
+    y_test = to_categorical(y_test)
+    print(">>> Data Loaded.")
+
+    print(">>> Start to train")
+    num_epochs = 10
+    for e in range(num_epochs):
+        print("\nTotal Epoch {}/{}".format(e+1, num_epochs))
+        history = test_model.fit(x=x_train, y=y_train,
+                                 shuffle=True, batch_size=128,
+                                 epochs=1, verbose=1,
+                                 validation_data=(x_test, y_test))
+        print("Training-Accuracy={}".format(history.history['acc'][-1]))
+        print("Training-Loss={}".format(history.history['loss'][-1]))
+        print("Validation-Accuracy={}".format(history.history['val_acc'][-1]))
+        print("Validation-Loss={}".format(history.history['val_loss'][-1]))
---
+++ examples/NAS-training-containers/cifar10/requirements.txt
@@ -0,0 +1,2 @@
+tensorflow-gpu
+keras
