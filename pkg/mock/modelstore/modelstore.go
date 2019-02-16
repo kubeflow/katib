d0779cb6f8a44b51664e02a11d8f3a8022d9058f
andreyvelich <andrey.velichkevich@gmail.com>
2019-02-15 18:14:48 -0800

    Generate new Mock files

--- pkg/mock/modelstore/modelstore.go
+++ pkg/mock/modelstore/modelstore.go
@@ -35,6 +35,7 @@ func (m *MockModelStore) EXPECT() *MockModelStoreMockRecorder {
 
 // GetSavedModel mocks base method
 func (m *MockModelStore) GetSavedModel(arg0 *api.GetSavedModelRequest) (*api.ModelInfo, error) {
+	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "GetSavedModel", arg0)
 	ret0, _ := ret[0].(*api.ModelInfo)
 	ret1, _ := ret[1].(error)
@@ -43,11 +44,13 @@ func (m *MockModelStore) GetSavedModel(arg0 *api.GetSavedModelRequest) (*api.Mod
 
 // GetSavedModel indicates an expected call of GetSavedModel
 func (mr *MockModelStoreMockRecorder) GetSavedModel(arg0 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetSavedModel", reflect.TypeOf((*MockModelStore)(nil).GetSavedModel), arg0)
 }
 
 // GetSavedModels mocks base method
 func (m *MockModelStore) GetSavedModels(arg0 *api.GetSavedModelsRequest) ([]*api.ModelInfo, error) {
+	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "GetSavedModels", arg0)
 	ret0, _ := ret[0].([]*api.ModelInfo)
 	ret1, _ := ret[1].(error)
@@ -56,11 +59,13 @@ func (m *MockModelStore) GetSavedModels(arg0 *api.GetSavedModelsRequest) ([]*api
 
 // GetSavedModels indicates an expected call of GetSavedModels
 func (mr *MockModelStoreMockRecorder) GetSavedModels(arg0 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetSavedModels", reflect.TypeOf((*MockModelStore)(nil).GetSavedModels), arg0)
 }
 
 // GetSavedStudies mocks base method
 func (m *MockModelStore) GetSavedStudies() ([]*api.StudyOverview, error) {
+	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "GetSavedStudies")
 	ret0, _ := ret[0].([]*api.StudyOverview)
 	ret1, _ := ret[1].(error)
@@ -69,11 +74,13 @@ func (m *MockModelStore) GetSavedStudies() ([]*api.StudyOverview, error) {
 
 // GetSavedStudies indicates an expected call of GetSavedStudies
 func (mr *MockModelStoreMockRecorder) GetSavedStudies() *gomock.Call {
+	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetSavedStudies", reflect.TypeOf((*MockModelStore)(nil).GetSavedStudies))
 }
 
 // SaveModel mocks base method
 func (m *MockModelStore) SaveModel(arg0 *api.SaveModelRequest) error {
+	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "SaveModel", arg0)
 	ret0, _ := ret[0].(error)
 	return ret0
@@ -81,11 +88,13 @@ func (m *MockModelStore) SaveModel(arg0 *api.SaveModelRequest) error {
 
 // SaveModel indicates an expected call of SaveModel
 func (mr *MockModelStoreMockRecorder) SaveModel(arg0 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "SaveModel", reflect.TypeOf((*MockModelStore)(nil).SaveModel), arg0)
 }
 
 // SaveStudy mocks base method
 func (m *MockModelStore) SaveStudy(arg0 *api.SaveStudyRequest) error {
+	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "SaveStudy", arg0)
 	ret0, _ := ret[0].(error)
 	return ret0
@@ -93,5 +102,6 @@ func (m *MockModelStore) SaveStudy(arg0 *api.SaveStudyRequest) error {
 
 // SaveStudy indicates an expected call of SaveStudy
 func (mr *MockModelStoreMockRecorder) SaveStudy(arg0 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "SaveStudy", reflect.TypeOf((*MockModelStore)(nil).SaveStudy), arg0)
 }
