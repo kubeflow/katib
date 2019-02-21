9dd5e6a4540e007b3f66f3fa310f3d45dfc70df8
John Platt <john.daniel.platt@gmail.com>
2019-02-21 12:07:10 -0500

    added discrete parameter to study config

--- pkg/suggestion/tests/test_algorithm_manager.py
+++ pkg/suggestion/tests/test_algorithm_manager.py
@@ -24,21 +24,23 @@ def observations():
 
 def test_algorithm_manager(study_config, observations):
     study_id = "test_id"
-    x_next = [1.0, 1.0]
+    x_next = [1.0, 1.0, 5]
     manager = AlgorithmManager(study_id, study_config,
                                observations.parameters, observations.metrics)
     assert manager.study_id == study_id
     assert manager.study_config == study_config
     assert manager.goal == api_pb2.MAXIMIZE
-    assert all(t == api_pb2.DOUBLE for t in manager.types)
-    assert manager.names == ["x", "y"]
-    assert manager.dim == 2
-    assert manager.lower_bound == [-5.0, -5.0]
-    assert manager.upper_bound == [5.0, 5.0]
-    assert np.allclose(manager.X_train, np.array([[1.0, 1.0], [1.0, 1.0]]))
+    assert manager.types == [api_pb2.DOUBLE, api_pb2.DOUBLE, api_pb2.DISCRETE]
+    assert manager.names == ["x", "y", "fake_discrete"]
+    assert manager.dim == 3
+    assert manager.lower_bound == [-5.0, -5.0, 2]
+    assert manager.upper_bound == [5.0, 5.0, 5]
+    assert manager.discrete_info == [{"name": "fake_discrete", "values": [2, 3, 5]}]
+    assert np.allclose(manager.X_train, np.array([[1.0, 1.0, 2], [1.0, 1.0, 3]]))
     assert np.allclose(manager.y_train, np.array([1.0, 1.0]))
     parsed_x_next = manager.parse_x_next(x_next)
     x_next_dict = manager.convert_to_dict(parsed_x_next)
     assert x_next_dict == \
            [{"name": "x", "value": 1.0, "type": api_pb2.DOUBLE},
-            {"name": "y", "value": 1.0, "type": api_pb2.DOUBLE}]
+            {"name": "y", "value": 1.0, "type": api_pb2.DOUBLE},
+            {"name": "fake_discrete", "value": 5, "type": api_pb2.DISCRETE}]
