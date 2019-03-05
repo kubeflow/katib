a393d4b825d49967a6fdbd26743cf9efe282d5c0
John Platt <john@carbonrelay.com>
2019-03-05 16:48:29 -0500

    updated manifests

--- cmd/suggestion/bayesianoptimization/main.py
+++ cmd/suggestion/bayesianoptimization/main.py
@@ -1,10 +1,10 @@
-import grpc
+import time
 from concurrent import futures
 
-import time
+import grpc
 
 from pkg.api.python import api_pb2_grpc
-from pkg.suggestion.bayesian_service import BayesianService
+from pkg.suggestion.bayesianoptimization.src.rpc_service import SuggestionService
 from pkg.suggestion.types import DEFAULT_PORT
 
 _ONE_DAY_IN_SECONDS = 60 * 60 * 24
@@ -12,7 +12,7 @@ _ONE_DAY_IN_SECONDS = 60 * 60 * 24
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    api_pb2_grpc.add_SuggestionServicer_to_server(BayesianService(), server)
+    api_pb2_grpc.add_SuggestionServicer_to_server(SuggestionService(), server)
     server.add_insecure_port(DEFAULT_PORT)
     print("Listening...")
     server.start()
