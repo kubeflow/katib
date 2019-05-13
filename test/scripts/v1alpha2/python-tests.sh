feb0f447cb9b3f102454132abafa9fb218e57c60
Ce Gao <gaoce@caicloud.io>
2019-05-13 10:23:04 +0800

    test: Remove develop
    Signed-off-by: Ce Gao <gaoce@caicloud.io>

--- test/scripts/v1alpha2/python-tests.sh
+++ test/scripts/v1alpha2/python-tests.sh
@@ -20,6 +20,5 @@ exit 0
 
 pip install -r cmd/suggestion/bayesianoptimization/requirements.txt
 pip install -r pkg/suggestion/test_requirements.txt
-python setup.py develop
 pylint pkg/suggestion/v1alpha1/bayesianoptimization/src --disable=fixme --exit-zero --reports=y
 pytest pkg/suggestion/v1alpha1/tests --verbose --cov=pkg/suggestion/bayesianoptimization/src --cov-report term-missing
