0f825ecc735334e3c05f2870427673baeec93f3d
Andrey Velichkevich <andrey.velichkevich@gmail.com>
2019-05-16 02:14:14 -0700

    Fix Status in Experiment Config

--- pkg/controller/v1alpha2/experiment/util/api_util.go
+++ pkg/controller/v1alpha2/experiment/util/api_util.go
@@ -77,7 +77,7 @@ func GetExperimentConf(instance *experimentsv1alpha2.Experiment) *api_pb.Experim
 				AlgorithmSetting: []*api_pb.AlgorithmSetting{},
 			},
 		},
-		ExperimentStatus: &api_pb.ExperimentStatus{
+		Status: &api_pb.ExperimentStatus{
 			StartTime:      convertTime2RFC3339(instance.Status.StartTime),
 			CompletionTime: convertTime2RFC3339(instance.Status.CompletionTime),
 			Condition:      getCondition(instance),
