daec513699c56332fd0fecf199d88565910bdb33
Achal Shantharam <ashantharam@ASHANTHARAM-mlt1.local>
2019-05-20 14:58:21 -0700

    Updating dbif reference

--- pkg/controller/v1alpha2/experiment/suggestion/fake/fake.go
+++ pkg/controller/v1alpha2/experiment/suggestion/fake/fake.go
@@ -2,7 +2,7 @@ package fake
 
 import (
 	experimentsv1alpha2 "github.com/kubeflow/katib/pkg/api/operators/apis/experiment/v1alpha2"
-	api_pb "github.com/kubeflow/katib/pkg/api/v1alpha2"
+	api_pb "github.com/kubeflow/katib/pkg/api/v1alpha2/dbif"
 	"github.com/kubeflow/katib/pkg/controller/v1alpha2/experiment/suggestion"
 )
 
