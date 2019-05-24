d43d8f16f0ce662131dd5b29f6111aa7032c5c82
Andrey Velichkevich <andrey.velichkevich@gmail.com>
2019-05-24 13:52:12 -0700

    Init commit

---
+++ cmd/suggestion/nasrl/v1alpha2/main.py
@@ -0,0 +1,28 @@
+import grpc
+from concurrent import futures
+
+import time
+
+from pkg.api.v1alpha2.python import api_pb2_grpc
+from pkg.suggestion.v1alpha2.nasrl_service import NasrlService
+from logging import getLogger, StreamHandler, INFO, DEBUG
+
+
+_ONE_DAY_IN_SECONDS = 60 * 60 * 24
+DEFAULT_PORT = "0.0.0.0:6789"
+
+def serve():
+    print("NAS RL Suggestion Service")
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    api_pb2_grpc.add_SuggestionServicer_to_server(NasrlService(), server)
+    server.add_insecure_port(DEFAULT_PORT)
+    print("Listening...")
+    server.start()
+    try:
+        while True:
+            time.sleep(_ONE_DAY_IN_SECONDS)
+    except KeyboardInterrupt:
+        server.stop(0)
+
+if __name__ == "__main__":
+    serve()
---
+++ pkg/suggestion/v1alpha2/temp.sh
@@ -0,0 +1,6 @@
+kubectl delete -f katib/manifests/v1alpha2/katib-controller
+kubectl delete -f katib/manifests/v1alpha2/katib/db
+kubectl delete -f katib/manifests/v1alpha2/katib/manager
+kubectl delete -f katib/manifests/v1alpha2/katib/manager-rest
+kubectl delete -f katib/manifests/v1alpha2/katib/pv
+kubectl delete -f katib/manifests/v1alpha2/katib/suggestion/random
