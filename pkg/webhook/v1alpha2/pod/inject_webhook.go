16d817505144d06b68873489e36b5f43d82912b2
hougang liu <liuhoug@cn.ibm.com>
2019-09-03 13:11:16 +0800

    MetricsCollector pod injection for katib job

--- pkg/webhook/v1alpha2/pod/inject_webhook.go
+++ pkg/webhook/v1alpha2/pod/inject_webhook.go
@@ -18,7 +18,6 @@ package pod
 
 import (
 	"context"
-	"errors"
 	"net/http"
 
 	logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
@@ -36,17 +35,6 @@ import (
 	katibmanagerv1alpha2 "github.com/kubeflow/katib/pkg/common/v1alpha2"
 )
 
-const (
-	// JobNameLabel represents the label key for the job name, the value is job name
-	JobNameLabel = "job-name"
-	// JobRoleLabel represents the label key for the job role, e.g. the value is master
-	JobRoleLabel = "job-role"
-	MasterRole   = "master"
-	// ControllerNameLabel represents the label key for the controller name, e.g. tf-operator and pytorch-operator
-	ControllerNameLabel = "controller-name"
-)
-
-// For debug
 var log = logf.Log.WithName("injector-webhook")
 
 // sidecarInjector that inject metrics collect sidecar into master pod
@@ -108,15 +96,15 @@ func NewSidecarInjector(c client.Client, ms string) *sidecarInjector {
 }
 
 func (s *sidecarInjector) MutationRequired(pod *v1.Pod, ns string) (bool, error) {
-	value, err := s.GetLabel(pod, JobRoleLabel)
-	if err != nil || value != MasterRole {
-		return false, nil
-	}
-
-	trialName, err := s.GetLabel(pod, JobNameLabel)
+	jobKind, jobName, err := getKabitJob(pod)
 	if err != nil {
 		return false, nil
 	}
+	if !isMasterRole(pod, jobKind) {
+		return false, nil
+	}
+
+	trialName := jobName
 	trial := &trialsv1alpha2.Trial{}
 	err = s.client.Get(context.TODO(), apitypes.NamespacedName{Name: trialName, Namespace: ns}, trial)
 	if err != nil {
@@ -130,43 +118,12 @@ func (s *sidecarInjector) MutationRequired(pod *v1.Pod, ns string) (bool, error)
 	return true, nil
 }
 
-func (s *sidecarInjector) GetLabel(pod *v1.Pod, targetLabel string) (string, error) {
-	labels := pod.Labels
-	for k, v := range labels {
-		if k == targetLabel {
-			return v, nil
-		}
-	}
-	return "", errors.New("Label " + targetLabel + " not found.")
-}
-
-func GetJobKind(controllerName string) string {
-	if controllerName == "tf-operator" {
-		return "TFJob"
-	} else if controllerName == "pytorch-operator" {
-		return "PyTorchJob"
-	}
-	return "Unknown"
-}
-
 func (s *sidecarInjector) Mutate(pod *v1.Pod, namespace string) (*v1.Pod, error) {
 	mutatedPod := pod.DeepCopy()
 
-	// Get job Kind from label controller-name.
-	kind := "TODO_Kind"
-	controllerName, err := s.GetLabel(pod, ControllerNameLabel)
-	if err == nil {
-		kind = GetJobKind(controllerName)
-	}
-
-	// Get the trial info from client
-	trialName, err := s.GetLabel(pod, JobNameLabel)
-	if err != nil {
-		return nil, err
-	}
+	kind, trialName, _ := getKabitJob(pod)
 	trial := &trialsv1alpha2.Trial{}
-	err = s.client.Get(context.TODO(), apitypes.NamespacedName{Name: trialName, Namespace: namespace}, trial)
-	if err != nil {
+	if err := s.client.Get(context.TODO(), apitypes.NamespacedName{Name: trialName, Namespace: namespace}, trial); err != nil {
 		return nil, err
 	}
 
@@ -195,5 +152,6 @@ func (s *sidecarInjector) Mutate(pod *v1.Pod, namespace string) (*v1.Pod, error)
 	mutatedPod.Spec.Containers = append(mutatedPod.Spec.Containers, injectContainer)
 	mutatedPod.Spec.ServiceAccountName = pod.Spec.ServiceAccountName
 
+	log.Info("Inject metrics collector sidecar container", "Pod", pod.Name, "Trial", trialName, "Experiment", experimentName)
 	return mutatedPod, nil
 }
