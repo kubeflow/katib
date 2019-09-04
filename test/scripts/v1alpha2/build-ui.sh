3184c008dd73b4ee53612ed0c2d379d99a73f4e3
Johnu George <johnugeorge109@gmail.com>
2019-09-04 11:37:56 +0530

    Adding extra timeouts to builds

--- test/scripts/v1alpha2/build-ui.sh
+++ test/scripts/v1alpha2/build-ui.sh
@@ -39,5 +39,5 @@ cp -r vendor ${GO_DIR}/vendor
 cd ${GO_DIR}
 
 cp cmd/ui/v1alpha2/Dockerfile .
-gcloud builds submit . --tag=${REGISTRY}/${REPO_NAME}/v1alpha2/katib-ui:${VERSION} --project=${PROJECT}
+gcloud builds submit . --tag=${REGISTRY}/${REPO_NAME}/v1alpha2/katib-ui:${VERSION} --project=${PROJECT} --timeout=20m
 gcloud container images add-tag --quiet ${REGISTRY}/${REPO_NAME}/v1alpha2/katib-ui:${VERSION} ${REGISTRY}/${REPO_NAME}/v1alpha2/katib-ui:latest --verbosity=info
