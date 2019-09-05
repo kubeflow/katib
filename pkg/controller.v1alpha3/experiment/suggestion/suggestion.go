744c64c9a053a9180dfa995b9cb687a760344d5d
Ce Gao <gaoce@caicloud.io>
2019-09-05 10:35:03 +0800

    fix: Remove the implementation
    Signed-off-by: Ce Gao <gaoce@caicloud.io>

--- pkg/controller.v1alpha3/experiment/suggestion/suggestion.go
+++ pkg/controller.v1alpha3/experiment/suggestion/suggestion.go
@@ -3,7 +3,6 @@ package suggestion
 import (
 	experimentsv1alpha3 "github.com/kubeflow/katib/pkg/apis/controller/experiments/v1alpha3"
 	api_pb "github.com/kubeflow/katib/pkg/apis/manager/v1alpha3"
-	common "github.com/kubeflow/katib/pkg/common/v1alpha3"
 )
 
 type Suggestion interface {
@@ -18,14 +17,15 @@ func New() Suggestion {
 }
 
 func (g *General) GetSuggestions(instance *experimentsv1alpha3.Experiment, addCount int32) ([]*api_pb.Trial, error) {
-	request := &api_pb.GetSuggestionsRequest{
-		ExperimentName: instance.Name,
-		AlgorithmName:  instance.Spec.Algorithm.AlgorithmName,
-		RequestNumber:  addCount,
-	}
-	if reply, err := common.GetSuggestions(request); err != nil {
-		return nil, err
-	} else {
-		return reply.Trials, nil
-	}
+	// request := &api_pb.GetSuggestionsRequest{
+	// 	ExperimentName: instance.Name,
+	// 	AlgorithmName:  instance.Spec.Algorithm.AlgorithmName,
+	// 	RequestNumber:  addCount,
+	// }
+	// if reply, err := common.GetSuggestions(request); err != nil {
+	// 	return nil, err
+	// } else {
+	// 	return reply.Trials, nil
+	// }
+	return nil, nil
 }
