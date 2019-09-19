6a033c4a3a8f907b183d9599804ae2dfdb1c2472
Ce Gao <gaoce@caicloud.io>
2019-09-19 12:16:42 +0800

    feat: Add bayesian
    Signed-off-by: Ce Gao <gaoce@caicloud.io>

---
+++ pkg/suggestion/v1alpha3/skopt/base_skopt_service.py
@@ -0,0 +1,97 @@
+import json
+import numpy as np
+import skopt
+import logging
+
+from pkg.suggestion.v1alpha3.internal.search_space import *
+from pkg.suggestion.v1alpha3.internal.trial import *
+
+logger = logging.getLogger("BaseSkoptService")
+
+
+class BaseSkoptService(object):
+    """
+    Refer to https://github.com/scikit-optimize/scikit-optimize .
+    """
+
+    def __init__(self, algorithm_name="skopt-bayesian-optimization",
+                 base_estimator="GP",
+                 n_initial_points=10,
+                 acq_func="gp_hedge",
+                 acq_optimizer="auto",
+                 random_state=None):
+        self.base_estimator = base_estimator
+        self.n_initial_points = n_initial_points
+        self.acq_func = acq_func
+        self.acq_optimizer = acq_optimizer
+        self.random_state = random_state
+        self.algorithm_name = algorithm_name
+
+    def getSuggestions(self, search_space, trials, request_number):
+        """
+        Get the new suggested trials with skopt algorithm.
+        """
+
+        skopt_search_space = []
+
+        for param in search_space.params:
+            if param.type == INTEGER:
+                skopt_search_space.append(skopt.space.Integer(
+                    int(param.min), int(param.max), name=param.name))
+            elif param.type == DOUBLE:
+                skopt_search_space.append(skopt.space.Real(
+                    float(param.min), float(param.max), "log-uniform", name=param.name))
+            elif param.type == CATEGORICAL or param.type == DISCRETE:
+                skopt_search_space.append(
+                    skopt.space.Categorical(param.list, name=param.name))
+
+        if self.algorithm_name != "skopt-bayesian-optimization":
+            raise Exception("Algorithm name is not supported by skopt service.")
+        skopt_optimizer = skopt.Optimizer(skopt_search_space,
+                                          base_estimator=self.base_estimator,
+                                          n_initial_points=self.n_initial_points,
+                                          acq_func=self.acq_func,
+                                          acq_optimizer=self.acq_optimizer,
+                                          random_state=self.random_state)
+
+        for trial in trials:
+            skopt_suggested = []
+            for param in search_space.params:
+                parameter_value = None
+                for assignment in trial.assignments:
+                    if assignment.name == param.name:
+                        parameter_value = assignment.value
+                        break
+                if param.type == INTEGER:
+                    skopt_suggested.append(int(parameter_value))
+                elif param.type == DOUBLE:
+                    skopt_suggested.append(float(parameter_value))
+                else:
+                    skopt_suggested.append(parameter_value)
+
+            loss_for_skopt = float(trial.target_metric.value)
+            if search_space.goal == MAX_GOAL:
+                loss_for_skopt = -1 * loss_for_skopt
+
+            skopt_optimizer.tell(skopt_suggested, loss_for_skopt)
+
+        return_trial_list = []
+
+        for i in range(request_number):
+            skopt_suggested = skopt_optimizer.ask()
+            return_trial_list.append(
+                BaseSkoptService.convert(search_space, skopt_suggested))
+        return return_trial_list
+
+    @staticmethod
+    def convert(search_space, skopt_suggested):
+        assignments = []
+        for i in range(len(search_space.params)):
+            param = search_space.params[i]
+            if param.type == INTEGER:
+                assignments.append(Assignment(param.name, skopt_suggested[i]))
+            elif param.type == DOUBLE:
+                assignments.append(Assignment(param.name, skopt_suggested[i]))
+            elif param.type == CATEGORICAL or param.type == DISCRETE:
+                assignments.append(Assignment(param.name, skopt_suggested[i]))
+        return assignments
---
+++ pkg/suggestion/v1alpha3/skopt_service.py
@@ -0,0 +1,79 @@
+import logging
+
+from pkg.apis.manager.v1alpha3.python import api_pb2
+from pkg.apis.manager.v1alpha3.python import api_pb2_grpc
+from pkg.suggestion.v1alpha3.internal.search_space import HyperParameter, HyperParameterSearchSpace
+from pkg.suggestion.v1alpha3.internal.trial import Trial, Assignment
+from pkg.suggestion.v1alpha3.skopt.base_skopt_service import BaseSkoptService
+
+logger = logging.getLogger("SkoptService")
+
+
+class SkoptService(
+        api_pb2_grpc.SuggestionServicer):
+    def GetSuggestions(self, request, context):
+        """
+        Main function to provide suggestion.
+        """
+        name, config = OptimizerConfiguration.convertAlgorithmSpec(
+            request.experiment.spec.algorithm)
+        base_service = BaseSkoptService(
+            algorithm_name=name,
+            base_estimator=config.base_estimator,
+            n_initial_points=config.n_initial_points,
+            acq_func=config.acq_func,
+            acq_optimizer=config.acq_optimizer,
+            random_state=config.random_state)
+        search_space = HyperParameterSearchSpace.convert(request.experiment)
+        trials = Trial.convert(request.trials)
+        new_trials = base_service.getSuggestions(
+            search_space, trials, request.request_number)
+        return api_pb2.GetSuggestionsReply(
+            parameter_assignments=Assignment.generate(new_trials)
+        )
+
+
+class OptimizerConfiguration(object):
+    def __init__(self, base_estimator="GP",
+                 n_initial_points=10,
+                 acq_func="gp_hedge",
+                 acq_optimizer="auto",
+                 random_state=None):
+        self.base_estimator = base_estimator
+        self.n_initial_points = n_initial_points
+        self.acq_func = acq_func
+        self.acq_optimizer = acq_optimizer
+        self.random_state = random_state
+
+    @staticmethod
+    def convertAlgorithmSpec(algorithm_spec):
+        optmizer = OptimizerConfiguration()
+        for s in algorithm_spec.algorithm_setting:
+            if s.name == "base_estimator":
+                optmizer.base_estimator = s.value
+            elif s.name == "n_initial_points":
+                optmizer.n_initial_points = int(s.value)
+            elif s.name == "acq_func":
+                optmizer.acq_func = s.value
+            elif s.name == "acq_optimizer":
+                optmizer.acq_optimizer = s.value
+            elif s.name == "random_state":
+                optmizer.random_state = int(s.value)
+        return algorithm_spec.algorithm_name, optmizer
+
+
+class HyperoptService(
+        api_pb2_grpc.SuggestionServicer):
+    def GetSuggestions(self, request, context):
+        """
+        Main function to provide suggestion.
+        """
+        base_serice = BaseHyperoptService(
+            algorithm_name=request.experiment.spec.algorithm.algorithm_name)
+        search_space = HyperParameterSearchSpace.convert(request.experiment)
+        trials = Trial.convert(request.trials)
+        new_assignments = base_serice.getSuggestions(
+            search_space, trials, request.request_number)
+        return api_pb2.GetSuggestionsReply(
+            parameter_assignments=Assignment.generate(new_assignments)
+        )
