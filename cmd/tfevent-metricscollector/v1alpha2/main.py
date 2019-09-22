4ff91ac86bdf1ee5a308f4201e368af2972db161
hougang liu <liuhgxa@gmail.com>
2019-09-22 23:02:00 +0800

    Implement tfevent collector

--- cmd/tfevent-metricscollector/v1alpha2/main.py
+++ cmd/tfevent-metricscollector/v1alpha2/main.py
@@ -10,10 +10,9 @@ def parse_options():
             description='TF-Event MetricsCollector',
             add_help = True
             )
-    parser.add_argument("-a", "--manager_addr", type = str, default = "katib-manager")
-    parser.add_argument("-p", "--manager_port", type = int, default = 6789 )
+    parser.add_argument("-s", "--manager_server_addr", type = str, default = "katib-manager:6789")
     parser.add_argument("-t", "--trial_name", type = str, default = "")
-    parser.add_argument("-d", "--log_dir", type = str, default = "/log")
+    parser.add_argument("-path", "--dir_path", type = str, default = "/log")
     parser.add_argument("-m", "--metric_names", type = str, default = "")
     opt = parser.parse_args()
     return opt
@@ -28,9 +27,14 @@ if __name__ == '__main__':
     opt = parse_options()
     
     mc = MetricsCollector(opt.metric_names.split(','))
-    observation_log = mc.parse_file(opt.log_dir)
+    observation_log = mc.parse_file(opt.dir_path)
 
-    channel = grpc.beta.implementations.insecure_channel(opt.manager_addr, opt.manager_port)
+    manager_server = opt.manager_server_addr.split(':')
+
+    if len(manager_server) != 2:
+        raise Exception("Invalid katib manager service address: %s" % opt.manager_server_addr)
+
+    channel = grpc.beta.implementations.insecure_channel(manager_server[0], int(manager_server[1]))
     with api_pb2.beta_create_Manager_stub(channel) as client:
         logger.info("In " + opt.trial_name + " " + str(len(observation_log.metric_logs)) + " metrics will be reported.")
         client.ReportObservationLog(api_pb2.ReportObservationLogRequest(
