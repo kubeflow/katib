adac23971fdc8f7348d1635d6c2f43b780947c8a
Ce Gao <gaoce@caicloud.io>
2019-09-24 14:46:50 +0800

    fix: Add output
    Signed-off-by: Ce Gao <gaoce@caicloud.io>

--- test/scripts/v1alpha3/run-suggestion-nasrl.sh
+++ test/scripts/v1alpha3/run-suggestion-nasrl.sh
@@ -62,5 +62,6 @@ cd ${GO_DIR}/test/e2e/v1alpha3
 echo "Running e2e NASRL experiment"
 export KUBECONFIG=$HOME/.kube/config
 go run run-e2e-experiment.go ../../../examples/v1alpha3/nasjob-example-RL.yaml
-
+kubectl -n kubeflow describe suggestion
+kubectl -n kubeflow delete experiment nas-rl-example
 exit 0
