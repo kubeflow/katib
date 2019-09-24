c84a8fdc3d1374f9a850b8830880a4dda443b9b3
Ce Gao <gaoce@caicloud.io>
2019-09-24 11:10:38 +0800

    fix: Fix
    Signed-off-by: Ce Gao <gaoce@caicloud.io>

--- cmd/suggestion/hyperband/v1alpha3/main.py
+++ cmd/suggestion/hyperband/v1alpha3/main.py
@@ -8,10 +8,12 @@ from concurrent import futures
 _ONE_DAY_IN_SECONDS = 60 * 60 * 24
 DEFAULT_PORT = "0.0.0.0:6789"
 
+
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     service = HyperbandService()
-    api_pb2_grpc.add_SuggestionServicer_to_server(service, server)    health_pb2_grpc.add_HealthServicer_to_server(service, server)
+    api_pb2_grpc.add_SuggestionServicer_to_server(service, server)
+    health_pb2_grpc.add_HealthServicer_to_server(service, server)
 
     server.add_insecure_port(DEFAULT_PORT)
     print("Listening...")
@@ -21,6 +23,7 @@ def serve():
             time.sleep(_ONE_DAY_IN_SECONDS)
     except KeyboardInterrupt:
         server.stop(0)
+
 
 if __name__ == "__main__":
     serve()
