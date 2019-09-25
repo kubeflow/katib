38ed84edaff3d5538a444e526754c44de0f6d173
Ce Gao <gaoce@caicloud.io>
2019-09-25 16:39:45 +0800

    fix: Remove updateSpec
    Signed-off-by: Ce Gao <gaoce@caicloud.io>

--- pkg/controller.v1alpha3/suggestion/suggestion_controller_status.go
+++ pkg/controller.v1alpha3/suggestion/suggestion_controller_status.go
@@ -17,15 +17,6 @@ const (
 	SuggestionKilledReason       = "SuggestionKilled"
 )
 
-func (r *ReconcileSuggestion) updateSpec(s *suggestionsv1alpha3.Suggestion, oldS *suggestionsv1alpha3.Suggestion) error {
-	if !equality.Semantic.DeepEqual(s.Spec, oldS.Spec) {
-		if err := r.Update(context.TODO(), s); err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
 func (r *ReconcileSuggestion) updateStatus(s *suggestionsv1alpha3.Suggestion, oldS *suggestionsv1alpha3.Suggestion) error {
 	if !equality.Semantic.DeepEqual(s.Status, oldS.Status) {
 		if err := r.Status().Update(context.TODO(), s); err != nil {
