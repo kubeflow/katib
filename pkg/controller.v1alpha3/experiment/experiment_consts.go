be5642f2313bde1a37ddb30fcceb93f3193da82e
Ce Gao <gaoce@caicloud.io>
2019-10-14 17:05:23 +0800

    feat: Add events in experiment
    Signed-off-by: Ce Gao <gaoce@caicloud.io>

---
+++ pkg/controller.v1alpha3/experiment/experiment_consts.go
@@ -0,0 +1,5 @@
+package experiment
+
+const (
+	ReconcileFailedReason = "ReconcileFailed"
+)
--- pkg/controller.v1alpha3/trial/trial_controller.go
+++ pkg/controller.v1alpha3/trial/trial_controller.go
@@ -56,11 +56,6 @@ var (
 	log = logf.Log.WithName(ControllerName)
 )
 
-/**
-* USER ACTION REQUIRED: This is a scaffold file intended for the user to modify with their own Controller
-* business logic.  Delete these comments after modifying this file.*
- */
-
 // Add creates a new Trial Controller and adds it to the Manager with default RBAC. The Manager will set fields on the Controller
 // and Start it when the Manager is Started.
 func Add(mgr manager.Manager) error {
