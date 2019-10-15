bad96263ad58380960db07bae0444abd664498f1
Ce Gao <gaoce@caicloud.io>
2019-10-14 22:37:53 -0500

    fix: Use as instead of , to support python 3 in tfevent metrics collector (#881)
    * fix: Use as instead of , to support python 3
    
    Signed-off-by: Ce Gao <gaoce@caicloud.io>
    
    * fix: Set a longer timeout
    
    Signed-off-by: Ce Gao <gaoce@caicloud.io>

--- cmd/metricscollector/v1alpha3/tfevent-metricscollector/main.py
+++ cmd/metricscollector/v1alpha3/tfevent-metricscollector/main.py
@@ -6,17 +6,22 @@ from pns import WaitOtherMainProcesses
 from tfevent_loader import MetricsCollector
 from logging import getLogger, StreamHandler, INFO
 
+timeout_in_seconds = 60
+
+
 def parse_options():
     parser = argparse.ArgumentParser(
-            description='TF-Event MetricsCollector',
-            add_help = True
-            )
-    parser.add_argument("-s", "--manager_server_addr", type = str, default = "katib-manager:6789")
-    parser.add_argument("-t", "--trial_name", type = str, default = "")
-    parser.add_argument("-path", "--dir_path", type = str, default = "/log")
-    parser.add_argument("-m", "--metric_names", type = str, default = "")
+        description='TF-Event MetricsCollector',
+        add_help=True
+    )
+    parser.add_argument("-s", "--manager_server_addr",
+                        type=str, default="katib-manager:6789")
+    parser.add_argument("-t", "--trial_name", type=str, default="")
+    parser.add_argument("-path", "--dir_path", type=str, default="/log")
+    parser.add_argument("-m", "--metric_names", type=str, default="")
     opt = parser.parse_args()
     return opt
+
 
 if __name__ == '__main__':
     logger = getLogger(__name__)
@@ -28,18 +33,21 @@ if __name__ == '__main__':
     opt = parse_options()
     manager_server = opt.manager_server_addr.split(':')
     if len(manager_server) != 2:
-        raise Exception("Invalid katib manager service address: %s" % opt.manager_server_addr)
-    
+        raise Exception("Invalid katib manager service address: %s" %
+                        opt.manager_server_addr)
+
     WaitOtherMainProcesses()
 
     mc = MetricsCollector(opt.metric_names.split(','))
     observation_log = mc.parse_file(opt.dir_path)
 
-    channel = grpc.beta.implementations.insecure_channel(manager_server[0], int(manager_server[1]))
+    channel = grpc.beta.implementations.insecure_channel(
+        manager_server[0], int(manager_server[1]))
 
     with api_pb2.beta_create_Manager_stub(channel) as client:
-        logger.info("In " + opt.trial_name + " " + str(len(observation_log.metric_logs)) + " metrics will be reported.")
+        logger.info("In " + opt.trial_name + " " +
+                    str(len(observation_log.metric_logs)) + " metrics will be reported.")
         client.ReportObservationLog(api_pb2.ReportObservationLogRequest(
             trial_name=opt.trial_name,
             observation_log=observation_log
-            ), 10)
+        ), timeout=timeout_in_seconds)
