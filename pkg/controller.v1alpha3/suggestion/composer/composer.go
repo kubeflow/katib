a65c960e8215a159fdf7329fb084cd68fd25b31b
Ce Gao <gaoce@caicloud.io>
2019-11-25 14:08:05 +0800

    fix: Format
    Signed-off-by: Ce Gao <gaoce@caicloud.io>

--- pkg/controller.v1alpha3/suggestion/composer/composer.go
+++ pkg/controller.v1alpha3/suggestion/composer/composer.go
@@ -3,10 +3,9 @@ package composer
 import (
 	"fmt"
 
-	"k8s.io/apimachinery/pkg/api/resource"
-
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
