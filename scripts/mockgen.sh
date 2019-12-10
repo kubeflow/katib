983583e3045f997a56b7a4b9557a53a791039167
Johnu George <johnugeorge109@gmail.com>
2019-12-10 07:29:28 +0530

    Delete v1alpha2 files (#953)

--- scripts/mockgen.sh
+++ scripts/mockgen.sh
@@ -21,23 +21,6 @@ set -o pipefail
 SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/..
 
 cd ${SCRIPT_ROOT}
-echo "Generating v1alpha2 ManagerClient..."
-mockgen -package mock -destination pkg/mock/v1alpha2/api/manager.go github.com/kubeflow/katib/pkg/apis/manager/v1alpha2 ManagerClient
-echo "Generating v1alpha2 SuggestionClient..."
-mockgen -package mock -destination pkg/mock/v1alpha2/api/suggestion.go github.com/kubeflow/katib/pkg/apis/manager/v1alpha2 SuggestionClient
-echo "Generating v1alpha2 KatibDBInterface..."
-mockgen -package mock -destination pkg/mock/v1alpha2/db/db.go github.com/kubeflow/katib/pkg/db/v1alpha2 KatibDBInterface
-echo "Generating v1alpha2 Generator..."
-mockgen -package mock -destination pkg/mock/v1alpha2/experiment/manifest/generator.go  github.com/kubeflow/katib/pkg/controller.v1alpha2/experiment/manifest Generator
-echo "Generating v1alpha2 KatibClient..."
-mockgen -package mock -destination pkg/mock/v1alpha2/util/katibclient/katibclient.go  github.com/kubeflow/katib/pkg/util/v1alpha2/katibclient Client
-echo "Generating v1alpha2 ManagerClient in Trial Controller..."
-mockgen -package mock -destination pkg/mock/v1alpha2/trial/managerclient/katibmanager.go  github.com/kubeflow/katib/pkg/controller.v1alpha2/trial/managerclient ManagerClient
-echo "Generating v1alpha2 ManagerClient in Experiment Controller..."
-mockgen -package mock -destination pkg/mock/v1alpha2/experiment/managerclient/managerclient.go  github.com/kubeflow/katib/pkg/controller.v1alpha2/experiment/managerclient ManagerClient
-echo "Generating v1alpha2 Suggestion in Experiment Controller..."
-mockgen -package mock -destination pkg/mock/v1alpha2/experiment/suggestion/suggestion.go  github.com/kubeflow/katib/pkg/controller.v1alpha2/experiment/suggestion Suggestion
-
 echo "Generating v1alpha3 ManagerClient..."
 mockgen -package mock -destination pkg/mock/v1alpha3/api/manager.go github.com/kubeflow/katib/pkg/apis/manager/v1alpha3 ManagerClient
 echo "Generating v1alpha3 SuggestionClient..."
