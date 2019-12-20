c8ce8510f487e11e7ad040b01eb78023a2de54d0
avelichk <andrey.velichkevich@gmail.com>
2019-12-20 15:59:19 +0000

    Remove redirect

--- pkg/ui/v1alpha3/frontend/src/components/KubeflowDashboard.jsx
+++ pkg/ui/v1alpha3/frontend/src/components/KubeflowDashboard.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import {CHANGE_GLOBAL_NAMESPACE} from '../actions/generalActions';
 import generalReducer from '../reducers/general';
 
+// TODO: Think about better way of update namespace
+// Right now, after updating namespace in select list
+// user must go to Kubeflow dashboard home page to update Global Namespace in Katib UI
 function onGlobalNamespaceChange(namespace) {
     generalReducer(undefined, {type: CHANGE_GLOBAL_NAMESPACE, globalNamespace: namespace})
-    window.location.replace(window.location.origin)
 }
 
 class KubeflowDashboard extends React.Component {
@@ -14,7 +16,7 @@ class KubeflowDashboard extends React.Component {
             if (window.centraldashboard && window.centraldashboard.CentralDashboardEventHandler) {
                 // Init method will invoke the callback with the event handler instance
                 // and a boolean indicating whether the page is iframed or not
-                window.centraldashboard.CentralDashboardEventHandler.init(function (cdeh, isIframed) {
+                window.centraldashboard.CentralDashboardEventHandler.init(function (cdeh) {
                         // Binds a callback that gets invoked anytime the Dashboard's
                         // namespace is changed
                         cdeh.onNamespaceSelected = (namespace) => {
