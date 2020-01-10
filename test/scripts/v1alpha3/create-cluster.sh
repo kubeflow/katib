ccbd475d16d8bb21ca329a7d1c33e4604eb1312c
avelichk <andrey.velichkevich@gmail.com>
2020-01-10 23:32:08 +0000

    Increase num nodes

--- test/scripts/v1alpha3/create-cluster.sh
+++ test/scripts/v1alpha3/create-cluster.sh
@@ -33,9 +33,9 @@ echo "Creating GPU cluster"
 gcloud --project ${PROJECT} beta container clusters create ${CLUSTER_NAME} \
     --zone ${ZONE} \
     --machine-type=n1-standard-8 \
-    --num-nodes=6 \
+    --num-nodes=8 \
     --enable-autoscaling \
-    --max-nodes=8 \
+    --max-nodes=10 \
     --cluster-version 1.14
 echo "Configuring kubectl"
 gcloud --project ${PROJECT} container clusters get-credentials ${CLUSTER_NAME} \
