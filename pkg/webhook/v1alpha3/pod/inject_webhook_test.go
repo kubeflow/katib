bc60a1509bfb188ab350d688ebaac890a04da7d1
Ce Gao <gaoce@caicloud.io>
2020-01-13 14:09:20 +0800

    fix: Add test cases
    Signed-off-by: Ce Gao <gaoce@caicloud.io>

--- pkg/webhook/v1alpha3/pod/inject_webhook_test.go
+++ pkg/webhook/v1alpha3/pod/inject_webhook_test.go
@@ -18,7 +18,127 @@ func TestWrapWorkerContainer(t *testing.T) {
 		MC            common.MetricsCollectorSpec
 		Expected      *v1.Pod
 		ExpectedError error
-	}{}
+		Name          string
+	}{
+		{
+			Pod: &v1.Pod{
+				Spec: v1.PodSpec{
+					Containers: []v1.Container{
+						{
+							Name: "tensorflow",
+							Command: []string{
+								"python main.py",
+							},
+						},
+					},
+				},
+			},
+			Namespace:   "nohere",
+			JobKind:     "TFJob",
+			MetricsFile: "testfile",
+			PathKind:    common.FileKind,
+			MC: common.MetricsCollectorSpec{
+				Collector: &common.CollectorSpec{
+					Kind: common.StdOutCollector,
+				},
+			},
+			Expected: &v1.Pod{
+				Spec: v1.PodSpec{
+					Containers: []v1.Container{
+						{
+							Name: "tensorflow",
+							Command: []string{
+								"sh", "-c",
+							},
+							Args: []string{
+								"python main.py 1>testfile 2>&1 && echo completed > $$$$.pid",
+							},
+						},
+					},
+				},
+			},
+			ExpectedError: nil,
+			Name:          "tensorflow container without sh -c",
+		},
+		{
+			Pod: &v1.Pod{
+				Spec: v1.PodSpec{
+					Containers: []v1.Container{
+						{
+							Name: "test",
+							Command: []string{
+								"python main.py",
+							},
+						},
+					},
+				},
+			},
+			Namespace:   "nohere",
+			JobKind:     "TFJob",
+			MetricsFile: "testfile",
+			PathKind:    common.FileKind,
+			MC: common.MetricsCollectorSpec{
+				Collector: &common.CollectorSpec{
+					Kind: common.StdOutCollector,
+				},
+			},
+			Expected: &v1.Pod{
+				Spec: v1.PodSpec{
+					Containers: []v1.Container{
+						{
+							Name: "test",
+							Command: []string{
+								"python main.py",
+							},
+						},
+					},
+				},
+			},
+			ExpectedError: nil,
+			Name:          "test container without sh -c",
+		},
+		{
+			Pod: &v1.Pod{
+				Spec: v1.PodSpec{
+					Containers: []v1.Container{
+						{
+							Name: "tensorflow",
+							Command: []string{
+								"sh", "-c",
+								"python main.py",
+							},
+						},
+					},
+				},
+			},
+			Namespace:   "nohere",
+			JobKind:     "TFJob",
+			MetricsFile: "testfile",
+			PathKind:    common.FileKind,
+			MC: common.MetricsCollectorSpec{
+				Collector: &common.CollectorSpec{
+					Kind: common.StdOutCollector,
+				},
+			},
+			Expected: &v1.Pod{
+				Spec: v1.PodSpec{
+					Containers: []v1.Container{
+						{
+							Name: "tensorflow",
+							Command: []string{
+								"sh", "-c",
+							},
+							Args: []string{
+								"python main.py 1>testfile 2>&1 && echo completed > $$$$.pid",
+							},
+						},
+					},
+				},
+			},
+			ExpectedError: nil,
+			Name:          "Tensorflow container with sh -c",
+		},
+	}
 
 	for _, c := range testCases {
 		err := wrapWorkerContainer(c.Pod, c.Namespace, c.JobKind, c.MetricsFile, c.PathKind, c.MC)
@@ -26,8 +146,9 @@ func TestWrapWorkerContainer(t *testing.T) {
 			t.Errorf("Expected error %v, got %v", c.ExpectedError, err)
 		}
 		if err == nil {
-			if !equality.Semantic.DeepEqual(c.Pod.Spec, c.Expected.Spec) {
-				t.Errorf("Expected pod %v, got %v", c.Pod.Spec, c.Expected.Spec)
+			if !equality.Semantic.DeepEqual(c.Pod.Spec.Containers, c.Expected.Spec.Containers) {
+				t.Errorf("Case %s: Expected pod %v, got %v",
+					c.Name, c.Expected.Spec.Containers, c.Pod.Spec.Containers)
 			}
 		}
 	}
