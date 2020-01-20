a49822c892377ff9591f81e351321fc96b5722c5
sperlingxx <lovedreamf@gmail.com>
2020-01-20 15:25:47 +0800

    fix

--- pkg/apis/controller/experiments/v1alpha3/util.go
+++ pkg/apis/controller/experiments/v1alpha3/util.go
@@ -102,7 +102,7 @@ func (exp *Experiment) IsCompletedReason(reason string) bool {
 }
 
 func (exp *Experiment) HasRunningTrials() bool {
-	return exp.Status.TrialsRunning != 0
+	return len(exp.Status.RunningTrials) != 0
 }
 
 func (exp *Experiment) GetLastConditionType() (ExperimentConditionType, error) {
