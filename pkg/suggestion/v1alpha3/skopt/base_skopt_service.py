ccfcdf94d70e80fed9b0c6ea4a9ce72ed23f43c0
avelichk <andrey.velichkevich@gmail.com>
2020-02-18 17:13:01 +0000

    Tell to Optimizer only about new Trials

--- pkg/suggestion/v1alpha3/skopt/base_skopt_service.py
+++ pkg/suggestion/v1alpha3/skopt/base_skopt_service.py
@@ -29,6 +29,9 @@ class BaseSkoptService(object):
         self.search_space = search_space
         self.skopt_optimizer = None
         self.create_optimizer()
+        self.succeeded_trials = 0
+        # Dict of recorded trials where key = loss value, value = List of trial assignment list for this value
+        self.recorded_trials = {}
 
     def create_optimizer(self):
         skopt_search_space = []
@@ -59,36 +62,48 @@ class BaseSkoptService(object):
 
         skopt_suggested = []
         loss_for_skopt = []
-        for trial in trials:
-            trial_assignment = []
-            for param in self.search_space.params:
-                parameter_value = None
-                for assignment in trial.assignments:
-                    if assignment.name == param.name:
-                        parameter_value = assignment.value
-                        break
-                if param.type == INTEGER:
-                    trial_assignment.append(int(parameter_value))
-                elif param.type == DOUBLE:
-                    trial_assignment.append(float(parameter_value))
-                else:
-                    trial_assignment.append(parameter_value)
-            skopt_suggested.append(trial_assignment)
-
-            loss_value = float(trial.target_metric.value)
-            if self.search_space.goal == MAX_GOAL:
-                loss_value = -1 * loss_value
-            loss_for_skopt.append(loss_value)
-
-        if loss_for_skopt != [] and skopt_suggested != []:
-            self.skopt_optimizer.tell(skopt_suggested, loss_for_skopt)
-
         return_trial_list = []
+        if len(trials) > self.succeeded_trials or self.succeeded_trials == 0:
+            self.succeeded_trials = len(trials)
+            logger.info("Succeeded Trials: {}".format(self.succeeded_trials))
+            for trial in trials:
+                trial_assignment = []
+                for param in self.search_space.params:
+                    parameter_value = None
+                    for assignment in trial.assignments:
+                        if assignment.name == param.name:
+                            parameter_value = assignment.value
+                            break
+                    if param.type == INTEGER:
+                        trial_assignment.append(int(parameter_value))
+                    elif param.type == DOUBLE:
+                        trial_assignment.append(float(parameter_value))
+                    else:
+                        trial_assignment.append(parameter_value)
 
-        for i in range(request_number):
-            skopt_suggested = self.skopt_optimizer.ask()
-            return_trial_list.append(
-                BaseSkoptService.convert(self.search_space, skopt_suggested))
+                loss_value = float(trial.target_metric.value)
+                if self.search_space.goal == MAX_GOAL:
+                    loss_value = -1 * loss_value
+
+                # Update only for new values
+                if loss_value not in self.recorded_trials:
+                    self.recorded_trials[loss_value] = []
+                    self.recorded_trials[loss_value].append(trial_assignment)
+                    skopt_suggested.append(trial_assignment)
+                    loss_for_skopt.append(loss_value)
+                elif trial_assignment not in self.recorded_trials[loss_value]:
+                    self.recorded_trials[loss_value].append(trial_assignment)
+                    skopt_suggested.append(trial_assignment)
+                    loss_for_skopt.append(loss_value)
+
+            if loss_for_skopt != [] and skopt_suggested != []:
+                self.skopt_optimizer.tell(skopt_suggested, loss_for_skopt)
+
+            for i in range(request_number):
+                skopt_suggested = self.skopt_optimizer.ask()
+                return_trial_list.append(
+                    BaseSkoptService.convert(self.search_space, skopt_suggested))
+
         return return_trial_list
 
     @staticmethod
@@ -102,4 +117,7 @@ class BaseSkoptService(object):
                 assignments.append(Assignment(param.name, skopt_suggested[i]))
             elif param.type == CATEGORICAL or param.type == DISCRETE:
                 assignments.append(Assignment(param.name, skopt_suggested[i]))
+        for a in assignments:
+            logger.info("Generate new Trial with Assignment")
+            logger.info(a)
         return assignments
