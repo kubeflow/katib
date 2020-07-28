b9e9dfbe1efb15c15dfa7171b55e8dd4ea509ac3
avelichk <andrey.velichkevich@gmail.com>
2020-07-28 19:28:35 +0100

    Run mock for new version

--- pkg/mock/v1beta1/suggestion/suggestionclient/suggestionclient.go
+++ pkg/mock/v1beta1/suggestion/suggestionclient/suggestionclient.go
@@ -12,30 +12,30 @@ import (
 	reflect "reflect"
 )
 
-// MockSuggestionClient is a mock of SuggestionClient interface
+// MockSuggestionClient is a mock of SuggestionClient interface.
 type MockSuggestionClient struct {
 	ctrl     *gomock.Controller
 	recorder *MockSuggestionClientMockRecorder
 }
 
-// MockSuggestionClientMockRecorder is the mock recorder for MockSuggestionClient
+// MockSuggestionClientMockRecorder is the mock recorder for MockSuggestionClient.
 type MockSuggestionClientMockRecorder struct {
 	mock *MockSuggestionClient
 }
 
-// NewMockSuggestionClient creates a new mock instance
+// NewMockSuggestionClient creates a new mock instance.
 func NewMockSuggestionClient(ctrl *gomock.Controller) *MockSuggestionClient {
 	mock := &MockSuggestionClient{ctrl: ctrl}
 	mock.recorder = &MockSuggestionClientMockRecorder{mock}
 	return mock
 }
 
-// EXPECT returns an object that allows the caller to indicate expected use
+// EXPECT returns an object that allows the caller to indicate expected use.
 func (m *MockSuggestionClient) EXPECT() *MockSuggestionClientMockRecorder {
 	return m.recorder
 }
 
-// SyncAssignments mocks base method
+// SyncAssignments mocks base method.
 func (m *MockSuggestionClient) SyncAssignments(arg0 *v1beta10.Suggestion, arg1 *v1beta1.Experiment, arg2 []v1beta11.Trial) error {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "SyncAssignments", arg0, arg1, arg2)
@@ -43,13 +43,13 @@ func (m *MockSuggestionClient) SyncAssignments(arg0 *v1beta10.Suggestion, arg1 *
 	return ret0
 }
 
-// SyncAssignments indicates an expected call of SyncAssignments
+// SyncAssignments indicates an expected call of SyncAssignments.
 func (mr *MockSuggestionClientMockRecorder) SyncAssignments(arg0, arg1, arg2 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "SyncAssignments", reflect.TypeOf((*MockSuggestionClient)(nil).SyncAssignments), arg0, arg1, arg2)
 }
 
-// ValidateAlgorithmSettings mocks base method
+// ValidateAlgorithmSettings mocks base method.
 func (m *MockSuggestionClient) ValidateAlgorithmSettings(arg0 *v1beta10.Suggestion, arg1 *v1beta1.Experiment) error {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "ValidateAlgorithmSettings", arg0, arg1)
@@ -57,7 +57,7 @@ func (m *MockSuggestionClient) ValidateAlgorithmSettings(arg0 *v1beta10.Suggesti
 	return ret0
 }
 
-// ValidateAlgorithmSettings indicates an expected call of ValidateAlgorithmSettings
+// ValidateAlgorithmSettings indicates an expected call of ValidateAlgorithmSettings.
 func (mr *MockSuggestionClientMockRecorder) ValidateAlgorithmSettings(arg0, arg1 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "ValidateAlgorithmSettings", reflect.TypeOf((*MockSuggestionClient)(nil).ValidateAlgorithmSettings), arg0, arg1)
