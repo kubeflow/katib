a7d80a3a2c3e845e5e2bcba7c921204ca65dc25a
avelichk <andrey.velichkevich@gmail.com>
2020-08-18 16:52:29 +0100

    Remove error

--- pkg/webhook/v1beta1/pod/inject_webhook.go
+++ pkg/webhook/v1beta1/pod/inject_webhook.go
@@ -117,7 +117,7 @@ func (s *sidecarInjector) MutationRequired(pod *v1.Pod, ns string) (bool, error)
 	// Try to get Katib job kind and job name from mutating pod
 	jobKind, jobName, err := s.getKatibJob(object, ns)
 	if err != nil {
-		return false, err
+		return false, nil
 	}
 
 	trial := &trialsv1beta1.Trial{}
