16ba3d313e3430ff42df90bdeb7767b68882ecfb
avelichk <andrey.velichkevich@gmail.com>
2020-08-28 21:25:06 +0100

    Fix condition

--- test/scripts/v1beta1/utils.sh
+++ test/scripts/v1beta1/utils.sh
@@ -5,21 +5,20 @@ _activate_service_account() {
   _max_attempts=5
   _is_activated=false
 
-  while [[ $_is_activated=false && $_count_attempts -le $_max_attempts ]]; do
+  while [[ $_is_activated = false && $_count_attempts -le $_max_attempts ]]; do
     echo "Activating service-account"
-    gcloud auth activate-service-account --key-file=${GOOGLE_APPLICATION_CREDENTIALS} && $_is_activated=true || _count_attempts=$((_count_attempts + 1))
+    gcloud auth activate-service-account --key-file=${GOOGLE_APPLICATION_CREDENTIALS} && _is_activated=true || _count_attempts=$((_count_attempts + 1))
 
-    if [ $_is_activated=false ]; then
+    if [ $_is_activated = false ]; then
       echo "gcloud activate service account failed, restart"
       sleep 1
     fi
 
   done
 
-  # If account was not activated exit the script
-  if [ $_is_activated=false ]; then
-    echo "Unable to activate gcloud service account!"
-    exit 0
+  # If account was not activated try to activate account last time.
+  if [ $_is_activated = false ]; then
+    gcloud auth activate-service-account --key-file=${GOOGLE_APPLICATION_CREDENTIALS}
   fi
 
 }
