614f9f8c7b5902a8145c50597b88063dd74b61a1
Andrey Velichkevich <andrey.velichkevich@gmail.com>
2020-10-27 03:15:59 +0000

    Remove istio sidecar annotation from all examples (#1338)
    * Manually add istio sidecar annotation to all examples
    
    * Revert pytorch namespace
    
    * Add istio to random
    
    * Remove istio annotation from examples
    
    * Remove istio from metadata example

--- pkg/controller.v1beta1/util/annotations.go
+++ pkg/controller.v1beta1/util/annotations.go
@@ -16,15 +16,10 @@ limitations under the License.
 package util
 
 import (
-	batchv1 "k8s.io/api/batch/v1"
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
-	"k8s.io/apimachinery/pkg/runtime"
 	logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
 
 	suggestionsv1beta1 "github.com/kubeflow/katib/pkg/apis/controller/suggestions/v1beta1"
 	"github.com/kubeflow/katib/pkg/controller.v1beta1/consts"
-	pytorchv1 "github.com/kubeflow/pytorch-operator/pkg/apis/pytorch/v1"
-	tfv1 "github.com/kubeflow/tf-operator/pkg/apis/tensorflow/v1"
 )
 
 var (
@@ -37,70 +32,6 @@ func SuggestionAnnotations(instance *suggestionsv1beta1.Suggestion) map[string]s
 		instance.Annotations,
 		consts.AnnotationIstioSidecarInjectName,
 		consts.AnnotationIstioSidecarInjectValue)
-}
-
-// TrainingJobAnnotations adds annotations to unstructured job.
-func TrainingJobAnnotations(desiredJob *unstructured.Unstructured) error {
-	kind := desiredJob.GetKind()
-	switch kind {
-	case consts.JobKindJob:
-		batchJob := &batchv1.Job{}
-		err := runtime.DefaultUnstructuredConverter.FromUnstructured(desiredJob.Object, &batchJob)
-		if err != nil {
-			log.Error(err, "Convert unstructured to job error")
-			return err
-		}
-		batchJob.Spec.Template.ObjectMeta.Annotations = appendAnnotation(
-			batchJob.Spec.Template.ObjectMeta.Annotations,
-			consts.AnnotationIstioSidecarInjectName,
-			consts.AnnotationIstioSidecarInjectValue)
-		desiredJob.Object, err = runtime.DefaultUnstructuredConverter.ToUnstructured(batchJob)
-		if err != nil {
-			log.Error(err, "Convert job to unstructured error")
-			return err
-		}
-		return nil
-	case consts.JobKindTF:
-		tfJob := &tfv1.TFJob{}
-		err := runtime.DefaultUnstructuredConverter.FromUnstructured(desiredJob.Object, &tfJob)
-		if err != nil {
-			log.Error(err, "Convert unstructured to TFJob error")
-			return err
-		}
-		for _, replicaSpec := range tfJob.Spec.TFReplicaSpecs {
-			replicaSpec.Template.ObjectMeta.Annotations = appendAnnotation(
-				replicaSpec.Template.ObjectMeta.Annotations,
-				consts.AnnotationIstioSidecarInjectName,
-				consts.AnnotationIstioSidecarInjectValue)
-		}
-		desiredJob.Object, err = runtime.DefaultUnstructuredConverter.ToUnstructured(tfJob)
-		if err != nil {
-			log.Error(err, "Convert TFJob to unstructured error")
-			return err
-		}
-		return nil
-	case consts.JobKindPyTorch:
-		pytorchJob := &pytorchv1.PyTorchJob{}
-		err := runtime.DefaultUnstructuredConverter.FromUnstructured(desiredJob.Object, &pytorchJob)
-		if err != nil {
-			log.Error(err, "Convert unstructured to PytorchJob error")
-			return err
-		}
-		for _, replicaSpec := range pytorchJob.Spec.PyTorchReplicaSpecs {
-			replicaSpec.Template.ObjectMeta.Annotations = appendAnnotation(
-				replicaSpec.Template.ObjectMeta.Annotations,
-				consts.AnnotationIstioSidecarInjectName,
-				consts.AnnotationIstioSidecarInjectValue)
-		}
-		desiredJob.Object, err = runtime.DefaultUnstructuredConverter.ToUnstructured(pytorchJob)
-		if err != nil {
-			log.Error(err, "Convert PytorchJob to unstructured error")
-			return err
-		}
-		return nil
-	default:
-		return nil
-	}
 }
 
 func appendAnnotation(annotations map[string]string, newAnnotationName string, newAnnotationValue string) map[string]string {
