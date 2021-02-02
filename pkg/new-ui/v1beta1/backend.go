1c0bf27c45195f0c1477e7f45bae03f763f45c01
Kimonas Sotirchos <kimwnasptd@arrikto.com>
2021-02-02 14:59:06 +0200

    Create a folder for the new-ui
    We will create a `new-ui` folder under the `pkg` dir to add the new UI.
    This will ensure that we won't break any existing functionality.
    
    Signed-off-by: Kimonas Sotirchos <kimwnasptd@arrikto.com>

---
+++ pkg/new-ui/v1beta1/backend.go
@@ -0,0 +1,344 @@
+package v1beta1
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/ghodss/yaml"
+	"google.golang.org/grpc"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	experimentv1beta1 "github.com/kubeflow/katib/pkg/apis/controller/experiments/v1beta1"
+	api_pb_v1beta1 "github.com/kubeflow/katib/pkg/apis/manager/v1beta1"
+	"github.com/kubeflow/katib/pkg/util/v1beta1/katibclient"
+)
+
+func NewKatibUIHandler(dbManagerAddr string) *KatibUIHandler {
+	kclient, err := katibclient.NewClient(client.Options{})
+	if err != nil {
+		log.Printf("NewClient for Katib failed: %v", err)
+		panic(err)
+	}
+	return &KatibUIHandler{
+		katibClient:   kclient,
+		dbManagerAddr: dbManagerAddr,
+	}
+}
+
+func (k *KatibUIHandler) connectManager() (*grpc.ClientConn, api_pb_v1beta1.DBManagerClient) {
+	conn, err := grpc.Dial(k.dbManagerAddr, grpc.WithInsecure())
+	if err != nil {
+		log.Printf("Dial to GRPC failed: %v", err)
+		return nil, nil
+	}
+	c := api_pb_v1beta1.NewDBManagerClient(conn)
+	return conn, c
+}
+
+func (k *KatibUIHandler) SubmitYamlJob(w http.ResponseWriter, r *http.Request) {
+	//enableCors(&w)
+	var data map[string]interface{}
+
+	json.NewDecoder(r.Body).Decode(&data)
+
+	job := experimentv1beta1.Experiment{}
+	if yamlContent, ok := data["yaml"].(string); ok {
+		err := yaml.Unmarshal([]byte(yamlContent), &job)
+		if err != nil {
+			log.Printf("Unmarshal YAML content failed: %v", err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		err = k.katibClient.CreateRuntimeObject(&job)
+		if err != nil {
+			log.Printf("CreateRuntimeObject from YAML failed: %v", err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+
+}
+
+func (k *KatibUIHandler) SubmitParamsJob(w http.ResponseWriter, r *http.Request) {
+	//enableCors(&w)
+	var data map[string]interface{}
+
+	json.NewDecoder(r.Body).Decode(&data)
+	if data, ok := data["postData"]; ok {
+		jsonbody, err := json.Marshal(data)
+		if err != nil {
+			log.Printf("Marshal data for experiment failed: %v", err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		job := experimentv1beta1.Experiment{}
+		if err := json.Unmarshal(jsonbody, &job); err != nil {
+			log.Printf("Unmarshal experiment failed: %v", err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		dataMap := data.(map[string]interface{})
+		job.TypeMeta = metav1.TypeMeta{
+			APIVersion: "kubeflow.org/v1beta1",
+			Kind:       "Experiment",
+		}
+		job.ObjectMeta = metav1.ObjectMeta{
+			Name:      dataMap["metadata"].(map[string]interface{})["name"].(string),
+			Namespace: dataMap["metadata"].(map[string]interface{})["namespace"].(string),
+		}
+		err = k.katibClient.CreateRuntimeObject(&job)
+		if err != nil {
+			log.Printf("CreateRuntimeObject from parameters failed: %v", err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+}
+
+// FetchAllExperiments gets HP and NAS experiments in all namespaces.
+func (k *KatibUIHandler) FetchAllExperiments(w http.ResponseWriter, r *http.Request) {
+	// At first, try to list experiments in cluster scope
+	experiments, err := k.getExperiments([]string{""})
+	if err != nil {
+		// If failed, just try to list experiments from own namespace
+		experiments, err = k.getExperiments([]string{})
+	}
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	response, err := json.Marshal(experiments)
+	if err != nil {
+		log.Printf("Marshal experiments failed: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Write(response)
+}
+
+func (k *KatibUIHandler) DeleteExperiment(w http.ResponseWriter, r *http.Request) {
+	experimentName := r.URL.Query()["experimentName"][0]
+	namespace := r.URL.Query()["namespace"][0]
+
+	experiment, err := k.katibClient.GetExperiment(experimentName, namespace)
+	if err != nil {
+		log.Printf("GetExperiment failed: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	err = k.katibClient.DeleteRuntimeObject(experiment)
+	if err != nil {
+		log.Printf("DeleteRuntimeObject failed: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	isExperimentDeleted := false
+
+	var experiments []ExperimentView
+
+	// Waiting until experiment will be deleted
+	for !isExperimentDeleted {
+		// At first, try to list experiments in cluster scope
+		experiments, err = k.getExperiments([]string{""})
+		if err != nil {
+			// If failed, just try to list experiments from own namespace
+			experiments, err = k.getExperiments([]string{})
+		}
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		isExperimentDeleted = true
+		for _, experiment := range experiments {
+			if experiment.Name == experimentName {
+				isExperimentDeleted = false
+				break
+			}
+		}
+	}
+
+	response, err := json.Marshal(experiments)
+	if err != nil {
+		log.Printf("Marshal HP and NAS experiments failed: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Write(response)
+}
+
+// FetchTrialTemplates gets all trial templates in all namespaces
+func (k *KatibUIHandler) FetchTrialTemplates(w http.ResponseWriter, r *http.Request) {
+
+	trialTemplatesViewList, err := k.getTrialTemplatesViewList()
+	if err != nil {
+		log.Printf("getTrialTemplatesViewList failed: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	TrialTemplatesResponse := TrialTemplatesResponse{
+		Data: trialTemplatesViewList,
+	}
+	response, err := json.Marshal(TrialTemplatesResponse)
+	if err != nil {
+		log.Printf("Marshal templates failed: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Write(response)
+}
+
+//AddTemplate adds template to ConfigMap
+func (k *KatibUIHandler) AddTemplate(w http.ResponseWriter, r *http.Request) {
+	var data map[string]interface{}
+	json.NewDecoder(r.Body).Decode(&data)
+
+	updatedConfigMapNamespace := data["updatedConfigMapNamespace"].(string)
+	updatedConfigMapName := data["updatedConfigMapName"].(string)
+	updatedConfigMapPath := data["updatedConfigMapPath"].(string)
+	updatedTemplateYaml := data["updatedTemplateYaml"].(string)
+
+	newTemplates, err := k.updateTrialTemplates(updatedConfigMapNamespace, updatedConfigMapName, "", updatedConfigMapPath, updatedTemplateYaml, ActionTypeAdd)
+	if err != nil {
+		log.Printf("updateTrialTemplates failed: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	TrialTemplatesResponse := TrialTemplatesResponse{
+		Data: newTemplates,
+	}
+	response, err := json.Marshal(TrialTemplatesResponse)
+	if err != nil {
+		log.Printf("Marhal failed: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Write(response)
+
+}
+
+// EditTemplate edits template in ConfigMap
+func (k *KatibUIHandler) EditTemplate(w http.ResponseWriter, r *http.Request) {
+
+	var data map[string]interface{}
+	json.NewDecoder(r.Body).Decode(&data)
+
+	updatedConfigMapNamespace := data["updatedConfigMapNamespace"].(string)
+	updatedConfigMapName := data["updatedConfigMapName"].(string)
+	configMapPath := data["configMapPath"].(string)
+	updatedConfigMapPath := data["updatedConfigMapPath"].(string)
+	updatedTemplateYaml := data["updatedTemplateYaml"].(string)
+
+	newTemplates, err := k.updateTrialTemplates(updatedConfigMapNamespace, updatedConfigMapName, configMapPath, updatedConfigMapPath, updatedTemplateYaml, ActionTypeEdit)
+	if err != nil {
+		log.Printf("updateTrialTemplates failed: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	TrialTemplatesResponse := TrialTemplatesResponse{
+		Data: newTemplates,
+	}
+	response, err := json.Marshal(TrialTemplatesResponse)
+	if err != nil {
+		log.Printf("Marhal failed: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Write(response)
+}
+
+// DeleteTemplate deletes template in ConfigMap
+func (k *KatibUIHandler) DeleteTemplate(w http.ResponseWriter, r *http.Request) {
+
+	var data map[string]interface{}
+	json.NewDecoder(r.Body).Decode(&data)
+
+	updatedConfigMapNamespace := data["updatedConfigMapNamespace"].(string)
+	updatedConfigMapName := data["updatedConfigMapName"].(string)
+	updatedConfigMapPath := data["updatedConfigMapPath"].(string)
+
+	newTemplates, err := k.updateTrialTemplates(updatedConfigMapNamespace, updatedConfigMapName, "", updatedConfigMapPath, "", ActionTypeDelete)
+	if err != nil {
+		log.Printf("updateTrialTemplates failed: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	TrialTemplatesResponse := TrialTemplatesResponse{
+		Data: newTemplates,
+	}
+
+	response, err := json.Marshal(TrialTemplatesResponse)
+	if err != nil {
+		log.Printf("Marhal failed: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Write(response)
+}
+
+func (k *KatibUIHandler) FetchNamespaces(w http.ResponseWriter, r *http.Request) {
+
+	// Get all available namespaces
+	namespaces, err := k.getAvailableNamespaces()
+	if err != nil {
+		log.Printf("GetAvailableNamespaces failed: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	response, err := json.Marshal(namespaces)
+	if err != nil {
+		log.Printf("Marshal namespaces failed: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Write(response)
+}
+
+// FetchExperiment gets experiment in specific namespace.
+func (k *KatibUIHandler) FetchExperiment(w http.ResponseWriter, r *http.Request) {
+	experimentName := r.URL.Query()["experimentName"][0]
+	namespace := r.URL.Query()["namespace"][0]
+
+	experiment, err := k.katibClient.GetExperiment(experimentName, namespace)
+	if err != nil {
+		log.Printf("GetExperiment failed: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	response, err := json.Marshal(experiment)
+	if err != nil {
+		log.Printf("Marshal Experiment failed: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Write(response)
+}
+
+// FetchSuggestion gets suggestion in specific namespace
+func (k *KatibUIHandler) FetchSuggestion(w http.ResponseWriter, r *http.Request) {
+	suggestionName := r.URL.Query()["suggestionName"][0]
+	namespace := r.URL.Query()["namespace"][0]
+
+	suggestion, err := k.katibClient.GetSuggestion(suggestionName, namespace)
+	if err != nil {
+		log.Printf("GetSuggestion failed: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	response, err := json.Marshal(suggestion)
+	if err != nil {
+		log.Printf("Marshal Suggestion failed: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Write(response)
+}
---
+++ pkg/new-ui/v1beta1/util.go
@@ -0,0 +1,274 @@
+package v1beta1
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"sort"
+	"strconv"
+	"strings"
+
+	"github.com/kubeflow/katib/pkg/controller.v1beta1/consts"
+
+	gographviz "github.com/awalterschulze/gographviz"
+	apiv1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func (k *KatibUIHandler) getExperiments(namespace []string) ([]ExperimentView, error) {
+	experiments := []ExperimentView{}
+
+	el, err := k.katibClient.GetExperimentList(namespace...)
+	if err != nil {
+		log.Printf("GetExperimentList failed: %v", err)
+		return nil, err
+	}
+	for _, experiment := range el.Items {
+		experimentLastCondition, err := experiment.GetLastConditionType()
+		if err != nil {
+			log.Printf("GetLastConditionType failed: %v", err)
+			return nil, err
+		}
+		newExperiment := ExperimentView{
+			Name:      experiment.Name,
+			Namespace: experiment.Namespace,
+			Status:    string(experimentLastCondition),
+		}
+
+		if experiment.Spec.NasConfig == nil {
+			newExperiment.Type = ExperimentTypeHP
+		} else {
+			newExperiment.Type = ExperimentTypeNAS
+		}
+
+		experiments = append(experiments, newExperiment)
+	}
+
+	return experiments, nil
+}
+
+func enableCors(w *http.ResponseWriter) {
+	(*w).Header().Set("Content-Type", "text/html; charset=utf-8")
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+	(*w).Header().Set("Access-Control-Allow-Headers", allowedHeaders)
+	(*w).Header().Set("Access-Control-Expose-Headers", "Access-Control-*")
+	(*w).Header().Set("Access-Control-Allow-Credentials", "true")
+}
+
+func (k *KatibUIHandler) getTrialTemplatesViewList() ([]TrialTemplatesDataView, error) {
+	trialTemplatesDataView := make([]TrialTemplatesDataView, 0)
+
+	// Get all available namespaces
+	namespaces, err := k.getAvailableNamespaces()
+	if err != nil {
+		log.Printf("GetAvailableNamespaces failed: %v", err)
+		return nil, err
+	}
+
+	// Get Trial Template ConfigMap for each namespace
+	for _, ns := range namespaces {
+		trialTemplatesConfigMapList, err := k.katibClient.GetTrialTemplates(ns)
+		if err != nil {
+			log.Printf("GetTrialTemplates failed: %v", err)
+			return nil, err
+		}
+
+		if len(trialTemplatesConfigMapList.Items) != 0 {
+			newTrialTemplatesView := getTrialTemplatesView(trialTemplatesConfigMapList)
+			// ConfigMap with templates must exists in namespace
+			if len(newTrialTemplatesView.ConfigMaps) > 0 {
+				trialTemplatesDataView = append(trialTemplatesDataView, newTrialTemplatesView)
+			}
+		}
+	}
+	return trialTemplatesDataView, nil
+}
+
+func (k *KatibUIHandler) getAvailableNamespaces() ([]string, error) {
+	var namespaces []string
+
+	namespaceList, err := k.katibClient.GetNamespaceList()
+	if err != nil {
+		namespaces = append(namespaces, consts.DefaultKatibNamespace)
+		return namespaces, nil
+	}
+	for _, ns := range namespaceList.Items {
+		namespaces = append(namespaces, ns.ObjectMeta.Name)
+	}
+
+	return namespaces, nil
+}
+
+func getTrialTemplatesView(templatesConfigMapList *apiv1.ConfigMapList) TrialTemplatesDataView {
+
+	trialTemplatesDataView := TrialTemplatesDataView{
+		ConfigMapNamespace: templatesConfigMapList.Items[0].ObjectMeta.Namespace,
+		ConfigMaps:         []ConfigMap{},
+	}
+	for _, configMap := range templatesConfigMapList.Items {
+		newConfigMap := ConfigMap{
+			ConfigMapName: configMap.ObjectMeta.Name,
+			Templates:     []Template{},
+		}
+		for key := range configMap.Data {
+			newTemplate := Template{
+				Path: key,
+				Yaml: configMap.Data[key],
+			}
+			newConfigMap.Templates = append(newConfigMap.Templates, newTemplate)
+		}
+
+		// Sort Trial Templates by Path
+		sort.SliceStable(newConfigMap.Templates, func(i, j int) bool {
+			return newConfigMap.Templates[i].Path <= newConfigMap.Templates[j].Path
+		})
+
+		// Templates with data must exists in ConfigMap
+		if len(newConfigMap.Templates) > 0 {
+			trialTemplatesDataView.ConfigMaps = append(trialTemplatesDataView.ConfigMaps, newConfigMap)
+		}
+
+	}
+	return trialTemplatesDataView
+}
+
+func (k *KatibUIHandler) updateTrialTemplates(
+	updatedConfigMapNamespace,
+	updatedConfigMapName,
+	configMapPath,
+	updatedConfigMapPath,
+	updatedTemplateYaml,
+	actionType string) ([]TrialTemplatesDataView, error) {
+
+	templates, err := k.katibClient.GetConfigMap(updatedConfigMapName, updatedConfigMapNamespace)
+	if err != nil && !(errors.IsNotFound(err) && actionType == ActionTypeAdd) {
+		log.Printf("GetConfigMap failed: %v", err)
+		return nil, err
+	}
+
+	if actionType == ActionTypeAdd {
+		if len(templates) == 0 {
+			templates = make(map[string]string)
+			templates[updatedConfigMapPath] = updatedTemplateYaml
+		} else {
+			templates[updatedConfigMapPath] = updatedTemplateYaml
+		}
+	} else if actionType == ActionTypeEdit {
+		delete(templates, configMapPath)
+		templates[updatedConfigMapPath] = updatedTemplateYaml
+	} else if actionType == ActionTypeDelete {
+		delete(templates, updatedConfigMapPath)
+	}
+
+	templatesConfigMap := &apiv1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      updatedConfigMapName,
+			Namespace: updatedConfigMapNamespace,
+			Labels:    TrialTemplateLabel,
+		},
+		Data: templates,
+	}
+
+	// If templates is empty delete Trial template configMap
+	if len(templates) == 0 {
+		err = k.katibClient.DeleteRuntimeObject(templatesConfigMap)
+		if err != nil {
+			log.Printf("DeleteRuntimeObject failed: %v", err)
+			return nil, err
+		}
+		// If len(templates) == 1 and adding template, we must create new ConfigMap
+	} else if len(templates) == 1 && actionType == ActionTypeAdd {
+		err = k.katibClient.CreateRuntimeObject(templatesConfigMap)
+		if err != nil {
+			log.Printf("CreateRuntimeObject failed: %v", err)
+			return nil, err
+		}
+		// Otherwise updating configMap
+	} else {
+		err = k.katibClient.UpdateRuntimeObject(templatesConfigMap)
+		if err != nil {
+			log.Printf("UpdateRuntimeObject failed: %v", err)
+			return nil, err
+		}
+	}
+
+	newTemplates, err := k.getTrialTemplatesViewList()
+	if err != nil {
+		log.Printf("getTrialTemplatesViewList: %v", err)
+		return nil, err
+	}
+
+	return newTemplates, nil
+
+}
+func getNodeString(block *Block) string {
+	var nodeString string
+	switch block.Type {
+	case "convolution":
+		nodeString += block.Param.FilterSize + "x" + block.Param.FilterSize
+		nodeString += " conv\n"
+		nodeString += block.Param.FilterSize + " channels"
+	case "separable_convolution":
+		nodeString += block.Param.FilterSize + "x" + block.Param.FilterSize
+		nodeString += " sep_conv\n"
+		nodeString += block.Param.FilterSize + " channels"
+	case "depthwise_convolution":
+		nodeString += block.Param.FilterSize + "x" + block.Param.FilterSize
+		nodeString += " depth_conv\n"
+	case "reduction":
+		// TODO: Need to be fixed
+		nodeString += "3x3 max_pooling"
+	}
+	return strconv.Quote(nodeString)
+}
+
+func generateNNImage(architecture string, decoder string) string {
+
+	var architectureInt [][]int
+
+	if err := json.Unmarshal([]byte(architecture), &architectureInt); err != nil {
+		panic(err)
+	}
+	/*
+		Always has num_layers, input_size, output_size and embeding
+		Embeding is a map: int to Parameter
+		Parameter has id, type, Option
+
+		Beforehand substite all ' to " and wrap the string in `
+	*/
+
+	replacedDecoder := strings.Replace(decoder, `'`, `"`, -1)
+	var decoderParsed Decoder
+
+	err := json.Unmarshal([]byte(replacedDecoder), &decoderParsed)
+	if err != nil {
+		panic(err)
+	}
+
+	graphAst, _ := gographviz.ParseString(`digraph G {}`)
+	graph := gographviz.NewGraph()
+	if err := gographviz.Analyse(graphAst, graph); err != nil {
+		panic(err)
+	}
+	graph.AddNode("G", "0", map[string]string{"label": strconv.Quote("Input")})
+	var i int
+	for i = 0; i < len(architectureInt); i++ {
+		graph.AddNode("G", strconv.Itoa(i+1), map[string]string{"label": getNodeString(decoderParsed.Embedding[architectureInt[i][0]])})
+		graph.AddEdge(strconv.Itoa(i), strconv.Itoa(i+1), true, nil)
+		for j := 1; j < i+1; j++ {
+			if architectureInt[i][j] == 1 {
+				graph.AddEdge(strconv.Itoa(j-1), strconv.Itoa(i+1), true, nil)
+			}
+		}
+	}
+	graph.AddNode("G", strconv.Itoa(i+1), map[string]string{"label": strconv.Quote("GlobalAvgPool")})
+	graph.AddEdge(strconv.Itoa(i), strconv.Itoa(i+1), true, nil)
+	graph.AddNode("G", strconv.Itoa(i+2), map[string]string{"label": strconv.Quote("FullConnect\nSoftmax")})
+	graph.AddEdge(strconv.Itoa(i+1), strconv.Itoa(i+2), true, nil)
+	graph.AddNode("G", strconv.Itoa(i+3), map[string]string{"label": strconv.Quote("Output")})
+	graph.AddEdge(strconv.Itoa(i+2), strconv.Itoa(i+3), true, nil)
+	s := graph.String()
+	return s
+}
