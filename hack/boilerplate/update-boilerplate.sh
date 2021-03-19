bc2fbd3368739c2cf7c16af09ac55fcf22a1eaaf
avelichk <andrey.velichkevich@gmail.com>
2021-03-19 22:46:01 +0000

    Modify script

--- hack/boilerplate/update-boilerplate.sh
+++ hack/boilerplate/update-boilerplate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # This script is used to update or add the boilerplate
-# to Python, Go files in ./pkg and ./cmd dir
+# to Python, Go files in ./pkg ./cmd and ./hack dirs.
 
 # Add boilerplate to Go files.
 # Exclude client, gRPC manager and mock dir from the search.
@@ -14,17 +14,15 @@ find_go_files=$(
 for i in ${find_go_files}; do
   # If the 2nd line starts with "Copyright" remove the boilerplate.
   if [[ $(head -n 2 $i | sed -n 2p) =~ "Copyright" ]]; then
-    test
-    # echo "Remove and add boilerplate to $i"
-    # tail -n +17 $i >$i.temp
+    echo "Remove the current boilerplate and add the new boilerplate to $i"
+    tail -n +17 $i >$i.temp
   # Only add boilerplate if the file doesn't have other license.
   elif ! grep -q "http://www.apache.org/licenses/LICENSE-2.0" $i; then
-    test
-    # echo "Add boilerplate to $i"
-    # cat $i >$i.temp
+    echo "Add the new boilerplate to $i"
+    cat $i >$i.temp
   else
-    echo "Existing lice $i"
+    echo "Skip file $i"
   fi
   # Add new boilerplate to the file.
-  # cat ./hack/boilerplate/boilerplate.go.txt $i.temp >$i && rm $i.temp
+  cat ./hack/boilerplate/boilerplate.go.txt $i.temp >$i && rm $i.temp
 done
