aeedf271d0a0c2688db0dd8ad784937dd4ba2337
avelichk <andrey.velichkevich@gmail.com>
2021-03-19 23:17:12 +0000

    Add script for Python files

--- hack/boilerplate/update-boilerplate.sh
+++ hack/boilerplate/update-boilerplate.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# This script is used to update or add the boilerplate
-# to Python, Go files in ./pkg ./cmd ./hack and ./test dirs.
+# This script is used to update or add the boilerplate in Go and Python files.
 
-# Add boilerplate to Go files.
-# Exclude client, gRPC manager, swagger, deepcopy and mock dir from the search.
+# Generate boilerplate for the Go files
+# in  ./cmd ./pkg ./hack and ./test dirs
+# Exclude client, gRPC manager, swagger, deepcopy and mock dirs from the search.
 find_go_files=$(
-  find ./pkg ./cmd ./hack ./test -name "*.go" \
+  find ./cmd ./pkg ./hack ./test -name "*.go" \
     ! -path "./pkg/client/*" \
     ! -path "./pkg/apis/manager/*" \
     ! -path "./pkg/apis/v1beta1/*" \
@@ -16,7 +16,7 @@ find_go_files=$(
 
 for i in ${find_go_files}; do
   # If the 2nd line starts with "Copyright" remove the current boilerplate.
-  if [[ $(head -n 2 $i | sed -n 2p) =~ "Copyright" ]]; then
+  if [[ $(sed -n 2p $i) =~ "Copyright" ]]; then
     echo "Remove the current boilerplate and add the new boilerplate to $i"
     tail -n +17 $i >$i.temp
   # Otherwise, add the new boilerplate to the file.
@@ -25,4 +25,26 @@ for i in ${find_go_files}; do
     cat $i >$i.temp
   fi
   cat ./hack/boilerplate/boilerplate.go.txt $i.temp >$i && rm $i.temp
+done
+
+# Generate boilerplate for the Python files
+# in ./pkg ./cmd ./hack and ./test
+# Exclude gRPC manager from the search
+find_python_files=$(
+  find ./cmd ./pkg ./hack ./test -name "*.py" \
+    ! -path "./pkg/apis/manager/*"
+)
+
+for i in ${find_python_files}; do
+  # If the 1st line starts with "# Copyright" remove the boilerplate.
+  if [[ $(sed -n 1p $i) =~ "# Copyright" ]]; then
+    echo "Remove the current boilerplate and add the new boilerplate to $i"
+    tail -n +15 $i >$i.temp
+  # Otherwise, add the new boilerplate to the file.
+  else
+    echo "Add the new boilerplate to $i"
+    cat $i >$i.temp
+  fi
+  # Add new boilerplate to the file.
+  cat ./hack/boilerplate/boilerplate.python.txt $i.temp >$i && rm $i.temp
 done
