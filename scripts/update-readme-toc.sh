8da9d77185fade101a8d0d85e9c5328d96a26918
avelichk <andrey.velichkevich@gmail.com>
2021-03-19 13:39:46 +0000

    Add Blog Posts in README

--- scripts/update-readme-toc.sh
+++ scripts/update-readme-toc.sh
@@ -3,5 +3,7 @@
 SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/..
 
 cd ${SCRIPT_ROOT}
-doctoc --github ./README.md --title "# Table of Contents"
+doctoc --github ./README.md --title "
+# Table of Contents"
+
 cd - >/dev/null
