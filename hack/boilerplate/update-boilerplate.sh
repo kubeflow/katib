5c232deb634336f3821eb076a37e3c8a603a2798
avelichk <andrey.velichkevich@gmail.com>
2021-03-19 21:31:10 +0000

    Modify boilerplate script for go files

--- hack/boilerplate/update-boilerplate.sh
+++ hack/boilerplate/update-boilerplate.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
 # This script is used to update or add the boilerplate
-# to Python, Go files in ./pkg and ./cmd dirs
+# to Python, Go files in ./pkg and ./cmd dir
 
-# for i in $(find ./cmd ./pkg -name "*.go"); do
-#   echo $i
-# done
-
-for i in $(find ./hack -name "*.go"); do
+# Add boilerplate to Go files
+for i in $(find ./hack ./cmd ./pkg -name "*.go"); do
   # If 2nd file line starts with "Copyright" remove the boilerplate.
   if [[ $(head -n 2 $i | sed -n 2p) =~ "Copyright" ]]; then
-    echo "Remove boilerplate from $i"
+    echo "Remove and add boilerplate to $i"
     tail -n +17 $i >$i.temp
   else
+    echo "Add boilerplate to $i"
     cat $i >$i.temp
   fi
   # Add new boilerplate to the file.
