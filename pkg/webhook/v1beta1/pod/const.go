011b296c151d703629f54fa805b650bd570b140a
Andrey Velichkevich <andrey.velichkevich@gmail.com>
2021-11-22 19:55:31 +0000

    Change namespace label for metrics collector injection

--- pkg/webhook/v1beta1/pod/const.go
+++ pkg/webhook/v1beta1/pod/const.go
@@ -30,7 +30,7 @@ const (
 )
 
 var (
-	NeedWrapWorkerMetricsCollecterList = [...]common.CollectorKind{
+	NeedWrapWorkerMetricsCollectorList = [...]common.CollectorKind{
 		common.StdOutCollector,
 		common.TfEventCollector,
 		common.FileCollector,
