d125f3754387f59001e1dc732e2473e37a9d3e78
tenzen-y <yuki.iwai.tz@gmail.com>
2021-11-24 01:02:25 +0900

    TFEventMetricsCollector supports TF>=2.0 and stop supporting TF <=1.x

---
+++ test/unit/v1beta1/metricscollector/test_tfevent_metricscollector.py
@@ -0,0 +1,46 @@
+# Copyright 2021 The Kubeflow Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import unittest
+import utils
+
+
+class TestTFEventMetricsCollector(unittest.TestCase):
+    def test_parse_file(self):
+
+        logs_dir = "./test/unit/v1beta1/metricscollector/testdata/tfevent-metricscollector/logs"
+
+        # Metric format is "{{dirname}}/{{metrics name}}"
+        metric_names = ["train/accuracy", "train/loss", "test/loss", "test/accuracy"]
+        metric_logs = utils.get_metric_logs(logs_dir, metric_names)
+        self.assertEqual(40, len(metric_logs))
+
+        for log in metric_logs:
+            metric_name = log["metric"]["name"]
+            self.assertIn(metric_name, metric_name)
+
+        # Metric format is "{{metrics name}}"
+        metric_names = ["accuracy", "loss"]
+        metrics_file_dir = os.path.join(logs_dir, "train")
+        metric_logs = utils.get_metric_logs(metrics_file_dir, metric_names)
+        self.assertEqual(20, len(metric_logs))
+
+        for log in metric_logs:
+            metric_name = log["metric"]["name"]
+            self.assertIn(metric_name, metric_name)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ test/unit/v1beta1/metricscollector/utils.py
@@ -0,0 +1,23 @@
+# Copyright 2021 The Kubeflow Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from tfevent_loader import MetricsCollector
+from google.protobuf import json_format
+
+
+def get_metric_logs(logs_dir, metric_names):
+    mc = MetricsCollector(metric_names)
+    observation_log = mc.parse_file(logs_dir)
+    dict_observation_log = json_format.MessageToDict(observation_log)
+    return dict_observation_log["metricLogs"]
