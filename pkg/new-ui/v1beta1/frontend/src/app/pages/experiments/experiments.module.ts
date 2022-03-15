68ec4190a5bab3fffdf0dd93de6cbd5c797ab101
Elena Zioga <elena@arrikto.com>
2022-03-15 14:55:36 +0200

    UI: Make KWA's main table responsive and add toolbar
    * Add a top row toolbar with the title of the app and the button to
      create a new Experiment.
    * Replace the card with a responsive table that shows the items. The
      component also has a paginator.
    
    Signed-off-by: Elena Zioga <elena@arrikto.com>

--- pkg/new-ui/v1beta1/frontend/src/app/pages/experiments/experiments.module.ts
+++ pkg/new-ui/v1beta1/frontend/src/app/pages/experiments/experiments.module.ts
@@ -1,28 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatCardModule } from '@angular/material/card';
-import {
-  NamespaceSelectModule,
-  ResourceTableModule,
-  ConfirmDialogModule,
-} from 'kubeflow';
-
+import { KubeflowModule } from 'kubeflow';
 import { ExperimentsComponent } from './experiments.component';
 import { ExperimentOptimalTrialModule } from './optimal-trial/experiment-optimal-trial.module';
 
 @NgModule({
   declarations: [ExperimentsComponent],
-  imports: [
-    CommonModule,
-    NamespaceSelectModule,
-    ResourceTableModule,
-
-    ConfirmDialogModule,
-    ExperimentOptimalTrialModule,
-    MatCardModule,
-    ConfirmDialogModule,
-    MatCardModule,
-  ],
+  imports: [CommonModule, ExperimentOptimalTrialModule, KubeflowModule],
   exports: [ExperimentsComponent],
 })
 export class ExperimentsModule {}
