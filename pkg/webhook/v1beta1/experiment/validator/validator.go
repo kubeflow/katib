07725af5758bdd48058f51e6926982c4ce3b149c
Aleksei Gorobets <agorobets@d2iq.com>
2022-04-15 18:37:33 -0400

    add handle Labels[label] and Annotations[annotation]

--- pkg/webhook/v1beta1/experiment/validator/validator.go
+++ pkg/webhook/v1beta1/experiment/validator/validator.go
@@ -311,10 +311,7 @@ func (g *DefaultValidator) validateTrialTemplate(instance *experimentsv1beta1.Ex
 
 		// Check if parameter reference exist in experiment parameters
 		if len(experimentParameterNames) > 0 {
-			// Check if parameter is trial metadata
-			regex := regexp.MustCompile(consts.TrialTemplateMetaReplaceFormatRegex)
-			match := regex.FindStringSubmatch(parameter.Reference)
-			if !(len(match) > 0 && contains(consts.TrialTemplateMetaKeys, match[1])) {
+			if !isMetaKey(parameter.Reference) {
 				if _, ok := experimentParameterNames[parameter.Reference]; !ok {
 					return fmt.Errorf("parameter reference %v does not exist in spec.parameters: %v", parameter.Reference, instance.Spec.Parameters)
 				}
@@ -485,6 +482,25 @@ func (g *DefaultValidator) validateMetricsCollector(inst *experimentsv1beta1.Exp
 	}
 
 	return nil
+}
+
+func isMetaKey(parameter string) bool {
+	// Check if parameter is trial metadata reference as ${trailSpec.Name}, ${trialSpec.Labels[label]}, etc. used for substitution
+	match := regexp.MustCompile(consts.TrialTemplateMetaReplaceFormatRegex).FindStringSubmatch(parameter)
+	isMeta := false
+	if len(match) > 0 {
+		matchedKey := match[1]
+		if contains(consts.TrialTemplateMetaKeys, matchedKey) {
+			isMeta = true
+		} else {
+			// Check if it's Labels[label] or Annotations[annotation]
+			subMatch := regexp.MustCompile(consts.TrialTemplateMetaParseFormatRegex).FindStringSubmatch(matchedKey)
+			if len(subMatch) == 3 && contains(consts.TrialTemplateMetaKeys, subMatch[1]) {
+				isMeta = true
+			}
+		}
+	}
+	return isMeta
 }
 
 func contains(slice []string, item string) bool {
