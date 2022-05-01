63170f5ee99df3c826f42624e6c9fd5e40a4409e
tenzen-y <yuki.iwai.tz@gmail.com>
2022-05-01 18:00:42 +0900

    fix scripts/v1beta1/release.sh

--- scripts/v1beta1/release.sh
+++ scripts/v1beta1/release.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Copyright 2021 The Kubeflow Authors.
 #
@@ -42,11 +42,11 @@ fi
 
 # Clone Katib repo to the temp dir.
 temp_dir=$(mktemp -d)
-git clone "git@github.com:kubeflow/katib.git" ${temp_dir}
-cd $temp_dir
+git clone "git@github.com:kubeflow/katib.git" "${temp_dir}"
+cd "$temp_dir"
 
 # Check if tag exists.
-if [[ ! -z $(git tag --list ${TAG}) ]]; then
+if [[ -n $(git tag --list "${TAG}") ]]; then
   echo "Tag: ${TAG} exists. Release can't be published"
   exit 1
 fi
@@ -54,13 +54,13 @@ fi
 echo -e "\nCreating a new release. Branch: ${BRANCH}, Tag: ${TAG}\n"
 
 # Create or use the branch.
-if [[ -z $(git branch -r -l origin/${BRANCH}) ]]; then
+if [[ -z $(git branch -r -l "origin/${BRANCH}") ]]; then
   echo "Branch: ${BRANCH} does not exist. Creating a new minor release"
-  git checkout -b ${BRANCH}
+  git checkout -b "${BRANCH}"
 else
   echo "Branch: ${BRANCH} exists. Creating a new patch release"
-  git checkout ${BRANCH}
-  read -p "Did you cherry pick all commits from the master to the ${BRANCH} branch? [y|n] "
+  git checkout "${BRANCH}"
+  read -rp "Did you cherry pick all commits from the master to the ${BRANCH} branch? [y|n] "
   if [ "$REPLY" != "y" ]; then
     exit 1
   fi
@@ -76,7 +76,7 @@ sdk_version=${TAG:1}
 # Check if this is pre-release.
 if [[ ${sdk_version} == *"-rc."* ]]; then
   # Replace "-rc." with "rc" for the SDK version.
-  sdk_version=$(sed "s@-rc.@rc@" <<<${sdk_version})
+  sdk_version=${sdk_version//-rc./rc}
 fi
 echo -e "\nPublishing Katib Python SDK, version: ${sdk_version}\n"
 # Run generate script.
@@ -99,22 +99,22 @@ echo -e "\nKatib Python SDK ${sdk_version} has been published\n"
 # ------------------ Commit changes ------------------
 git commit -a -m "Katib official release ${TAG}"
 # Create a new tag.
-git tag ${TAG}
+git tag "${TAG}"
 
 # ------------------ Publish Katib images ------------------
 # Publish images to the registry with 2 tags: ${TAG} and v1beta1-<commit-sha>
 echo -e "\nPublishing Katib images\n"
-make push-tag TAG=${TAG}
+make push-tag TAG="${TAG}"
 echo -e "Katib images have been published\n"
 
 # ------------------ Push to upstream ------------------
-read -p "Do you want to push Katib ${TAG} version to the upstream? [y|n] "
+read -rp "Do you want to push Katib ${TAG} version to the upstream? [y|n] "
 if [ "$REPLY" != "y" ]; then
   exit 1
 fi
 # Push a new Branch and Tag.
-git push -u origin ${BRANCH}
-git push -u origin ${TAG}
+git push -u origin "${BRANCH}"
+git push -u origin "${TAG}"
 
 echo -e "\nKatib ${TAG} release has been published"
 echo "To finish the release process, follow these steps: https://github.com/kubeflow/katib/tree/master/docs/release#create-a-new-katib-release"
