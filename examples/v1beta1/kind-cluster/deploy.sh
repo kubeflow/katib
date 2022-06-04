5f2b50184d0f6ed0cc79fd11fcc1812b8068ed67
tenzen-y <yuki.iwai.tz@gmail.com>
2022-06-04 17:56:37 +0900

    fix shellcheck

--- examples/v1beta1/kind-cluster/deploy.sh
+++ examples/v1beta1/kind-cluster/deploy.sh
@@ -18,19 +18,19 @@
 set -e
 
 # Verify that appropriate tools are installed.
-if [[ ! $(which docker) ]]; then
+if [ -z "$(command -v docker)" ]; then
   echo "Unable to find Docker"
   echo "To install Docker, please follow this guide: https://docs.docker.com/get-docker"
   exit 1
 fi
 
-if [[ ! $(which kind) ]]; then
+if [ -z "$(command -v kind)" ]; then
   echo "Unable to find Kind"
   echo "To install Kind, please follow this guide: https://kind.sigs.k8s.io/docs/user/quick-start/#installation"
   exit 1
 fi
 
-if [[ ! $(which kubectl) ]]; then
+if [ -z "$(command -v kubectl)" ]; then
   echo "Unable to find kubectl"
   echo "To install kubectl, please follow this guide: https://kubernetes.io/docs/tasks/tools/#kubectl"
   exit 1
