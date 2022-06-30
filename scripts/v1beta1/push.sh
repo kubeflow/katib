8dcc7d3398f75ab00c7cb0e757e9491dc40f8a7e
Johnu George <johnugeorge109@gmail.com>
2022-06-30 18:54:17 +0530

    Fix push script to include new images (#1912)

--- scripts/v1beta1/push.sh
+++ scripts/v1beta1/push.sh
@@ -107,8 +107,11 @@ docker push "${REGISTRY}/enas-cnn-cifar10-gpu:${TAG}"
 echo -e "\nPushing Keras CIFAR-10 CNN training container example for ENAS with CPU support...\n"
 docker push "${REGISTRY}/enas-cnn-cifar10-cpu:${TAG}"
 
-echo -e "\nPushing PyTorch CIFAR-10 CNN training container example for DARTS...\n"
-docker push "${REGISTRY}/darts-cnn-cifar10:${TAG}"
+echo -e "\nPushing PyTorch CIFAR-10 CNN training container example for DARTS with CPU support...\n"
+docker push "${REGISTRY}/darts-cnn-cifar10-cpu:${TAG}"
+
+echo -e "\nPushing PyTorch CIFAR-10 CNN training container example for DARTS with GPU support...\n"
+docker push "${REGISTRY}/darts-cnn-cifar10-gpu:${TAG}"
 
 echo -e "\nPushing dynamic learning rate training container example for PBT...\n"
 docker push "${REGISTRY}/simple-pbt:${TAG}"
