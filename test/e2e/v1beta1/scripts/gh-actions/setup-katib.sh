504fa1ce99136b0014ae1d9b1e1551eb3aec6910
Yuki Iwai <yuki.iwai.tz@gmail.com>
2023-08-13 21:39:17 +0900

    E2E: Add additional check to verify if the components are ready
    Signed-off-by: Yuki Iwai <yuki.iwai.tz@gmail.com>

--- test/e2e/v1beta1/scripts/gh-actions/setup-katib.sh
+++ test/e2e/v1beta1/scripts/gh-actions/setup-katib.sh
@@ -66,7 +66,8 @@ cd ../../../../../ && WITH_DATABASE_TYPE=$WITH_DATABASE_TYPE make deploy && cd -
 
 # Wait until all Katib pods is running.
 TIMEOUT=120s
-kubectl wait --for=condition=ready --timeout=${TIMEOUT} -l "katib.kubeflow.org/component in ($WITH_DATABASE_TYPE,controller,db-manager,ui)" -n kubeflow pod ||
+
+kubectl wait --for=condition=ContainersReady=True --timeout=${TIMEOUT} -l "katib.kubeflow.org/component in ($WITH_DATABASE_TYPE,controller,db-manager,ui)" -n kubeflow pod ||
   (kubectl get pods -n kubeflow && kubectl describe pods -n kubeflow && exit 1)
 
 echo "All Katib components are running."
