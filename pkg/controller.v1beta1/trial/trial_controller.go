4d1d010c6a606ab70bc4b78cbf355feed81b09da
Electronic-Waste <2690692950@qq.com>
2024-08-02 20:59:34 +0000

    fix(trial): fix lint error in controller.
    Signed-off-by: Electronic-Waste <2690692950@qq.com>

--- pkg/controller.v1beta1/trial/trial_controller.go
+++ pkg/controller.v1beta1/trial/trial_controller.go
@@ -248,8 +248,8 @@ func (r *ReconcileTrial) reconcileTrial(instance *trialsv1beta1.Trial) error {
 		// If observation is empty, metrics collector doesn't finish.
 		// For early stopping scenario, metrics collector will report logs before Trial status is changed to EarlyStopped.
 		// We need to requeue reconcile when the Trial is succeeded, metrics collector's type is not `Push`, and metrics are not reported.
-		if jobStatus.Condition == trialutil.JobSucceeded && 
-			instance.Status.Observation == nil && 
+		if jobStatus.Condition == trialutil.JobSucceeded &&
+			instance.Status.Observation == nil &&
 			instance.Spec.MetricsCollector.Collector.Kind != commonapiv1beta1.PushCollector {
 			logger.Info("Trial job is succeeded but metrics are not reported, reconcile requeued")
 			return errMetricsNotReported
