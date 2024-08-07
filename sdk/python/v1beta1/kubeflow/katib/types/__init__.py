af534b36d12a4292a79a6a1b6cee7ca79c0fd171
helenxie-bit <helenxiehz@gmail.com>
2024-08-07 11:53:21 +0800

    fix Lint errors
    Signed-off-by: helenxie-bit <helenxiehz@gmail.com>

--- sdk/python/v1beta1/kubeflow/katib/types/__init__.py
+++ sdk/python/v1beta1/kubeflow/katib/types/__init__.py
@@ -2,6 +2,5 @@ from __future__ import absolute_import
 
 # Import types into type package
 from kubeflow.katib.types.trainer_resources import TrainerResources
-
 # Import Kubernetes models.
 from kubernetes.client import *
