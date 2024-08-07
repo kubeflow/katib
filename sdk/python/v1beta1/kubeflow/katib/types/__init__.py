c7f6e10125413332ebd9835f9901dd7345bb445b
helenxie-bit <helenxiehz@gmail.com>
2024-08-07 11:55:32 +0800

    delete types
    Signed-off-by: helenxie-bit <helenxiehz@gmail.com>

--- sdk/python/v1beta1/kubeflow/katib/types/__init__.py
+++
@@ -1,6 +0,0 @@
-from __future__ import absolute_import
-
-# Import types into type package
-from kubeflow.katib.types.trainer_resources import TrainerResources
-# Import Kubernetes models.
-from kubernetes.client import *
