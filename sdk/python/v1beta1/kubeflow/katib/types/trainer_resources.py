1db907ec9f816593fff5fee14937adfa9af061cd
helenxie-bit <helenxiehz@gmail.com>
2024-08-19 17:35:20 +0800

    fix format
    Signed-off-by: helenxie-bit <helenxiehz@gmail.com>

--- sdk/python/v1beta1/kubeflow/katib/types/trainer_resources.py
+++ sdk/python/v1beta1/kubeflow/katib/types/trainer_resources.py
@@ -111,9 +111,11 @@ class TrainerResources(object):
             elif isinstance(value, dict):
                 result[attr] = dict(
                     map(
-                        lambda item: (item[0], item[1].to_dict())
-                        if hasattr(item[1], "to_dict")
-                        else item,
+                        lambda item: (
+                            (item[0], item[1].to_dict())
+                            if hasattr(item[1], "to_dict")
+                            else item
+                        ),
                         value.items(),
                     )
                 )
