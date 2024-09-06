f9438cbe30b942e3c04ad64fe46d998c43f242f8
shivas1516 <sivasubramaniaml.ai2021@dce.edu.in>
2024-09-06 19:36:43 +0530

    Update to ensure linting and Go testing are performed
    Signed-off-by: shivas1516 <sivasubramaniaml.ai2021@dce.edu.in>

--- docs/sdkdocs/source/conf.py
+++ docs/sdkdocs/source/conf.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+
 sys.path.insert(0, os.path.abspath('../../../sdk/python/v1beta1/kubeflow/katib'))
 
 # For the full list of built-in configuration values, see the documentation:
