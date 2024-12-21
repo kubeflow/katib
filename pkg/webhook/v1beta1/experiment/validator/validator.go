5aa332d6d4d1bca78311df8764dc37b48f183560
Aydan Pirani <aydanpirani@gmail.com>
2024-12-20 23:09:01 -0800

    Set experiment names at a max of 40 characters. If len(algorithm) + len(experiment-name) > 63, then the suggestion service cannot start.
    Signed-off-by: Aydan Pirani <aydanpirani@gmail.com>

--- pkg/webhook/v1beta1/experiment/validator/validator.go
+++ pkg/webhook/v1beta1/experiment/validator/validator.go
@@ -82,10 +82,11 @@ func (g *DefaultValidator) ValidateExperiment(instance, oldInst *experimentsv1be
 	var allErrs field.ErrorList
 
 	namingConvention, _ := regexp.Compile("^[a-z]([-a-z0-9]*[a-z0-9])?")
-	if !namingConvention.MatchString(instance.Name) {
+	if !namingConvention.MatchString(instance.Name) || len(instance.Name) > 40 {
 		msg := "name must consist of lower case alphanumeric characters or '-'," +
 			" start with an alphabetic character, and end with an alphanumeric character" +
-			" (e.g. 'my-name', or 'abc-123', regex used for validation is '^[a-z]([-a-z0-9]*[a-z0-9])?)'"
+			" (e.g. 'my-name', or 'abc-123', regex used for validation is '^[a-z]([-a-z0-9]*[a-z0-9])?)'" +
+			" and may not be larger than 40 characters. "
 
 		allErrs = append(allErrs, field.Invalid(field.NewPath("metadata").Child("name"), instance.Name, msg))
 	}
