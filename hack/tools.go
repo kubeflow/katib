0de551daeb9db9050f778889e1327b10e5ca985b
Electronic-Waste <2690692950@qq.com>
2025-01-09 06:11:41 +0000

    fix(codegen): fix package dependency on k8s.io/code-generator.
    Signed-off-by: Electronic-Waste <2690692950@qq.com>

--- hack/tools.go
+++ hack/tools.go
@@ -21,10 +21,6 @@ limitations under the License.
 package test
 
 import (
-	_ "k8s.io/code-generator/cmd/client-gen"
-	_ "k8s.io/code-generator/cmd/deepcopy-gen"
-	_ "k8s.io/code-generator/cmd/defaulter-gen"
-	_ "k8s.io/code-generator/cmd/informer-gen"
-	_ "k8s.io/code-generator/cmd/lister-gen"
+	_ "k8s.io/code-generator"
 	_ "k8s.io/kube-openapi/cmd/openapi-gen"
 )
