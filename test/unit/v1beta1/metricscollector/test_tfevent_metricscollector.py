9f0e32fefd8168db33a989648d7cc7cd124b31a7
Gary Miguel <garymm@garymm.org>
2025-02-06 09:44:41 -0800

    test
    Signed-off-by: Gary Miguel <garymm@garymm.org>

--- test/unit/v1beta1/metricscollector/test_tfevent_metricscollector.py
+++ test/unit/v1beta1/metricscollector/test_tfevent_metricscollector.py
@@ -13,10 +13,21 @@
 # limitations under the License.
 
 import os
+import tempfile
 import unittest
+
+import tensorboardX
 
 import utils
 
+
+METRIC_DIR_NAMES = ("train", "test")
+METRIC_NAMES = ("accuracy", "loss")
+QUALIFIED_METRIC_NAMES = tuple(
+    f"{dir}/{metric}"
+    for dir in METRIC_DIR_NAMES
+    for metric in METRIC_NAMES
+)
 
 class TestTFEventMetricsCollector(unittest.TestCase):
     def test_parse_file(self):
@@ -24,24 +35,47 @@ class TestTFEventMetricsCollector(unittest.TestCase):
         current_dir = os.path.dirname(os.path.abspath(__file__))
         logs_dir = os.path.join(current_dir, "testdata/tfevent-metricscollector/logs")
 
-        # Metric format is "{{dirname}}/{{metrics name}}"
-        metric_names = ["train/accuracy", "train/loss", "test/loss", "test/accuracy"]
-        metric_logs = utils.get_metric_logs(logs_dir, metric_names)
+
+        metric_logs = utils.get_metric_logs(logs_dir, QUALIFIED_METRIC_NAMES)
         self.assertEqual(20, len(metric_logs))
 
         for log in metric_logs:
             actual = log["metric"]["name"]
-            self.assertIn(actual, metric_names)
+            self.assertIn(actual, QUALIFIED_METRIC_NAMES)
 
-        # Metric format is "{{metrics name}}"
-        metric_names = ["accuracy", "loss"]
-        metrics_file_dir = os.path.join(logs_dir, "train")
-        metric_logs = utils.get_metric_logs(metrics_file_dir, metric_names)
-        self.assertEqual(10, len(metric_logs))
+        train_metric_logs = utils.get_metric_logs(
+            os.path.join(logs_dir, "train"), METRIC_NAMES)
+        self.assertEqual(10, len(train_metric_logs))
+
+        for log in train_metric_logs:
+            actual = log["metric"]["name"]
+            self.assertIn(actual, METRIC_NAMES)
+
+    def test_parse_file_with_tensorboardX(self):
+        logs_dir = tempfile.mkdtemp()
+        num_iters = 3
+
+        for dir_name in METRIC_DIR_NAMES:
+            with tensorboardX.SummaryWriter(os.path.join(logs_dir, dir_name)) as writer:
+                for metric_name in METRIC_NAMES:
+                    for iter in range(num_iters):
+                        writer.add_scalar(metric_name, 0.1, iter)
+
+
+        metric_logs = utils.get_metric_logs(logs_dir, QUALIFIED_METRIC_NAMES)
+        self.assertEqual(num_iters * len(QUALIFIED_METRIC_NAMES), len(metric_logs))
 
         for log in metric_logs:
             actual = log["metric"]["name"]
-            self.assertIn(actual, metric_names)
+            self.assertIn(actual, QUALIFIED_METRIC_NAMES)
+
+        train_metric_logs = utils.get_metric_logs(
+            os.path.join(logs_dir, "train"), METRIC_NAMES)
+        self.assertEqual(num_iters * len(METRIC_NAMES), len(train_metric_logs))
+
+        for log in train_metric_logs:
+            actual = log["metric"]["name"]
+            self.assertIn(actual, METRIC_NAMES)
 
 
 if __name__ == '__main__':
