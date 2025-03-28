663cacf0c5f9ad56a7213b0cb6047b07dc935e82
helenxie-bit <helenxiehz@gmail.com>
2025-03-27 21:37:39 -0700

    fix the error of setup
    Signed-off-by: helenxie-bit <helenxiehz@gmail.com>

--- sdk/python/v1beta1/setup.py
+++ sdk/python/v1beta1/setup.py
@@ -88,7 +88,7 @@ setuptools.setup(
     install_requires=REQUIRES,
     extras_require={
         "huggingface": [
-            "kubeflow-training[huggingface]==1.9.0",
+            "kubeflow-training==1.9.0",
             "transformers==4.50.2",
             "peft==0.15.1",
         ],
