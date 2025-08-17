d563290cb504c8391c1e61187e605e7297d270ec
wassimbensalem <bswassim@gmail.com>
2025-08-17 17:33:00 +0200

    test: use pytests

--- sdk/python/v1beta1/kubeflow/katib/utils/utils_test.py
+++ sdk/python/v1beta1/kubeflow/katib/utils/utils_test.py
@@ -1,37 +1,18 @@
-import unittest
-
+import pytest
 from kubeflow.katib.utils import utils
 
-
-class TestUtils(unittest.TestCase):
-    def test_format_pip_index_urls(self):
-        # Test with a single URL.
-        pip_index_urls = ["https://pypi.org/simple"]
-        self.assertEqual(
-            utils.format_pip_index_urls(pip_index_urls),
-            "--index-url https://pypi.org/simple",
-        )
-
-        # Test with multiple URLs.
-        pip_index_urls = ["https://pypi.org/simple", "https://private-repo.com/simple"]
-        self.assertEqual(
-            utils.format_pip_index_urls(pip_index_urls),
-            "--index-url https://pypi.org/simple --extra-index-url https://private-repo.com/simple",
-        )
-
-        # Test with three URLs.
-        pip_index_urls = ["https://pypi.org/simple", "https://private-repo.com/simple", "https://another-repo.com/simple"]
-        self.assertEqual(
-            utils.format_pip_index_urls(pip_index_urls),
-            "--index-url https://pypi.org/simple --extra-index-url https://private-repo.com/simple --extra-index-url https://another-repo.com/simple",
-        )
-
-        # Test with default value.
-        self.assertEqual(
-            utils.format_pip_index_urls(),
-            "--index-url https://pypi.org/simple",
-        )
-
-
-if __name__ == "__main__":
-    unittest.main()
+@pytest.mark.parametrize(
+    "pip_index_urls, expected",
+    [
+        (["https://pypi.org/simple"],
+         "--index-url https://pypi.org/simple"),
+        (["https://pypi.org/simple", "https://private-repo.com/simple"],
+         "--index-url https://pypi.org/simple --extra-index-url https://private-repo.com/simple"),
+        (["https://pypi.org/simple", "https://private-repo.com/simple", "https://another-repo.com/simple"],
+         "--index-url https://pypi.org/simple --extra-index-url https://private-repo.com/simple --extra-index-url https://another-repo.com/simple"),
+        (None,
+         "--index-url https://pypi.org/simple"),
+    ]
+)
+def test_format_pip_index_urls(pip_index_urls, expected):
+    assert utils.format_pip_index_urls(pip_index_urls) == expected
