ae8ee026672c5848f252992a669eb3e08aaa551e
Andrey Velichkevich <andrey.velichkevich@gmail.com>
2025-10-30 20:36:29 +0000

    feat: add extra index urls to the katib tune
    Signed-off-by: Andrey Velichkevich <andrey.velichkevich@gmail.com>

---
+++ sdk/python/v1beta1/kubeflow/katib/utils/utils_test.py
@@ -0,0 +1,26 @@
+import pytest
+from kubeflow.katib.utils import utils
+
+
+@pytest.mark.parametrize(
+    "pip_index_urls, expected",
+    [
+        (["https://pypi.org/simple"], "--index-url https://pypi.org/simple"),
+        (
+            ["https://pypi.org/simple", "https://private-repo.com/simple"],
+            "--index-url https://pypi.org/simple --extra-index-url https://private-repo.com/simple",
+        ),
+        (
+            [
+                "https://pypi.org/simple",
+                "https://private-repo.com/simple",
+                "https://another-repo.com/simple",
+            ],
+            "--index-url https://pypi.org/simple --extra-index-url https://private-repo.com/simple "
+            "--extra-index-url https://another-repo.com/simple",
+        ),
+        (None, "--index-url https://pypi.org/simple"),
+    ],
+)
+def test_format_pip_index_urls(pip_index_urls, expected):
+    assert utils.format_pip_index_urls(pip_index_urls) == expected
